986a42512c2feeba9232426912fe6f660a0e2b7f
Chris Smith <chris@pulumi.com>
2018-03-02 15:25:40 -0800

    Publish doc builds to "s3://rel.pulumi.com/docs/${ENVIRONMENT}"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,14 +13,14 @@ if [ -z ${ENVIRONMENT} ]; then
     exit 1
 fi
 
-echo "Assuming the role of UploadPulumiReleases, uploading ${CURRENT_COMMIT:0:6}..."
+echo "Assuming the role of UploadPulumiReleases, uploading ${ENVIRONMENT} at ${CURRENT_COMMIT:0:6}..."
 CREDS_JSON=$(aws sts assume-role \
     --role-arn "arn:aws:iam::058607598222:role/UploadPulumiReleases" \
-    --role-session-name "docs-release_${CURRENT_COMMIT:0:6}" \
+    --role-session-name "docs-release_${ENVIRONMENT}_${CURRENT_COMMIT:0:6}" \
     --external-id "upload-docs-release")
 
 export AWS_ACCESS_KEY_ID=$(echo ${CREDS_JSON}     | jq ".Credentials.AccessKeyId" --raw-output)
 export AWS_SECRET_ACCESS_KEY=$(echo ${CREDS_JSON} | jq ".Credentials.SecretAccessKey" --raw-output)
 export AWS_SECURITY_TOKEN=$(echo ${CREDS_JSON}    | jq ".Credentials.SessionToken" --raw-output)
 
-aws s3 sync ./_site/ s3://rel.pulumi.com/docs/${ENVIRONMENT}/${CURRENT_COMMIT}
+aws s3 sync ./_site/ s3://rel.pulumi.com/docs/${ENVIRONMENT}
