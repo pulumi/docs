70637ee9ecfce00cd22403d5b2a17949b49e6264
joeduffy <joe@pulumi.com>
2018-05-24 19:48:56 -0700

    Add an API Doc Generation tool
    This change includes a simple tool that takes Typedoc files -- generated
    from our JSDoc comments in our TypeScript packages -- and creates API
    Documentation in Markdown suitable for our docs website.
    
    The bulk of this tool is the interpretation of Typedoc's JSON
    representation.  From there, we simply raise the ASTs into a slightly
    higher level module representation.  In this, we "cheat" a bit, so that
    the documentation is tailored to the idiomatic Pulumi module structure.
    This is largely why we can generate better docs than Typedoc does.
    (yay, ES6 modules...)
    
    It is, as a result, not infinitely generalizable.
    
    We use Mustache templates so that editing the Markdown results is
    reasonable straightforward, although, I must warn, the whitespace
    sensitivity is a little cumbersome.  That said, way better than
    maintaining YAHWCG (yet-another-hand-written-code-generator).
    
    There are plenty of improvements on their way, but this is a significant
    improvement to what we've got -- including deep linking to GitHub!!! --
    so I'm going to land now and incrementally refine with feedback in hand.

---
+++ tools/tscdocgen/template.go
@@ -0,0 +1,136 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/cbroglie/mustache"
+)
+
+var indexTemplate, _ = mustache.ParseString(`---
+title: {{Title}}
+---
+
+{{#Breadcrumbs}}{{{.}}}{{/Breadcrumbs}}
+{{#Package}}
+Node.js:
+
+` + "```" + `javascript
+var {{PackageVar}} = require("{{Package}}");
+` + "```" + `
+
+ES6 modules:
+
+` + "```" + `typescript
+import * as {{PackageVar}} from "{{Package}}";
+` + "```" + `
+{{/Package}}
+
+<h2 class="pdoc-module-header">Index</h2>
+
+{{#Members}}
+* <a href="#{{Name}}">{{Label}}</a>
+{{/Members}}
+
+{{#Files}}<a href="{{RepoURL}}/{{.}}">{{.}}</a> {{/Files}}
+
+<h2 class="pdoc-module-header">Modules</h2>
+
+{{#Modules}}
+* <a href="{{.}}">{{.}}</a>
+{{/Modules}}
+
+{{#Members}}
+<h2 class="pdoc-module-header" id="{{Name}}">
+<a class="pdoc-member-name" href="{{RepoURL}}">{{{Label}}}</a>
+</h2>
+{{#DetailedLabel}}
+
+` + "```typescript" + `
+{{{DetailedLabel}}}
+` + "```" + `
+
+{{/DetailedLabel}}
+{{#Comment.ShortText}}
+
+{{{Comment.ShortText}}}
+
+{{#Comment.Text}}
+{{{Comment.Text}}}
+{{/Comment.Text}}
+{{/Comment.ShortText}}
+{{#Children}}
+<h3 class="pdoc-member-header">
+<a class="pdoc-child-name" href="{{RepoURL}}">{{{Label}}}</a>
+</h3>
+{{#DetailedLabel}}
+
+` + "```typescript" + `
+{{{DetailedLabel}}}
+` + "```" + `
+
+{{/DetailedLabel}}
+{{#Signatures}}
+
+` + "```typescript" + `
+{{{Label}}}
+` + "```" + `
+
+{{#Comment.ShortText}}
+
+{{{Comment.ShortText}}}
+
+{{#Comment.Text}}
+{{{Comment.Text}}}
+{{/Comment.Text}}
+{{/Comment.ShortText}}
+{{#Parameters}}
+{{#Comment.ShortText}}
+* ` + "`{{Name}}`" + ` {{Comment.ShortText}}
+{{/Comment.ShortText}}
+{{/Parameters}}
+{{/Signatures}}
+{{^Signatures}}
+{{#Comment.ShortText}}
+
+{{{Comment.ShortText}}}
+
+{{#Comment.Text}}
+{{{Comment.Text}}}
+{{/Comment.Text}}
+{{/Comment.ShortText}}
+{{/Signatures}}
+{{/Children}}
+{{#Signatures}}
+
+` + "```typescript" + `
+{{{Label}}}
+` + "```" + `
+
+{{#Comment.ShortText}}
+
+{{{Comment.ShortText}}}
+
+{{#Comment.Text}}
+{{{Comment.Text}}}
+{{/Comment.Text}}
+{{/Comment.ShortText}}
+{{#Parameters}}
+{{#Comment.ShortText}}
+* ` + "`{{Name}}`" + ` {{Comment.ShortText}}
+{{/Comment.ShortText}}
+{{/Parameters}}
+{{/Signatures}}
+{{/Members}}
+`)
