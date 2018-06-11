ed9a32488525e3bd07824a2fe646cbb1a1dabdea
Chris Smith <chrsmith@users.noreply.github.com>
2018-06-11 08:53:51 -0700

    Standup production stack of docs site (#365)

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -170,6 +170,12 @@ function getDomainAndSubdomain(domain: string): { subdomain: string, parentDomai
     if (parts.length < 2) {
         throw new Error(`No TLD found on ${domain}`);
     }
+    if (parts.length === 2) {
+        return {
+            subdomain: "",
+            parentDomain: domain + ".",
+        };
+    }
 
     const subdomain = parts[0];
     parts.shift();  // Drop first element.
