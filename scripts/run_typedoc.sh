b3bcde09778bcd31906ba577b21547fd71694f16
Sean Gillespie <sean@pulumi.com>
2018-06-14 12:46:01 -0700

    Run typedoc on aws-infra and aws-serverless

--- scripts/run_typedoc.sh
+++ scripts/run_typedoc.sh
@@ -64,6 +64,22 @@ cd ../pulumi-gcp/sdk/nodejs
 $TYPEDOC --json $PULUMI_DOCS/pulumi-gcp.docs.json --mode modules --includeDeclarations --excludeExternals
 popd
 
+# pulumi-aws-infra
+echo -e "\033[0;95mrunning typedoc on pulumi-aws-infra\033[0m"
+pushd .
+cd ../pulumi-aws-infra/nodejs/aws-infra
+$TYPEDOC --json $PULUMI_DOCS/pulumi-aws-infra.docs.json --mode modules --includeDeclarations --excludeExternals
+popd
+
+# pulumi-aws-serverless
+echo -e "\033[0;95mrunning typedoc on pulumi-aws-serverless\033[0m"
+pushd .
+cd ../pulumi-aws-serverless/nodejs/aws-serverless
+$TYPEDOC --json $PULUMI_DOCS/pulumi-aws-serverless.docs.json --mode modules --includeDeclarations --excludeExternals
+popd
+
+
+
 echo "Finished running typedoc. Generating update docs..."
 TSC_DOCGEN="go run ./tools/tscdocgen/*.go"
 PKG_DOCS=./reference/pkg/nodejs/@pulumi
@@ -75,5 +91,7 @@ $TSC_DOCGEN $PULUMI_DOCS/pulumi-cloud-aws.docs.json $PKG_DOCS/cloud-aws
 $TSC_DOCGEN $PULUMI_DOCS/pulumi-azure.docs.json $PKG_DOCS/azure
 $TSC_DOCGEN $PULUMI_DOCS/pulumi-kubernetes.docs.json $PKG_DOCS/kubernetes
 $TSC_DOCGEN $PULUMI_DOCS/pulumi-gcp.docs.json $PKG_DOCS/gcp
+$TSC_DOCGEN $PULUMI_DOCS/pulumi-aws-infra.docs.json $PKG_DOCS/aws-infra
+$TSC_DOCGEN $PULUMI_DOCS/pulumi-aws-serverless.docs.json $PKG_DOCS/aws-serverless
 
 echo "Done"
