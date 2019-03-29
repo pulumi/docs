e69cec8394c414edfd16e198d3ca5ea6fa192e6d
Levi Blackstone <levi@pulumi.com>
2019-03-29 11:58:10 -0600

    Cleanup run_typedoc.sh
    - Make bash code more robust
    - Alphabetize list of providers

--- scripts/run_typedoc.sh
+++ scripts/run_typedoc.sh
@@ -25,35 +25,34 @@ PULUMI_DOC_BASE=./reference/pkg/nodejs/@pulumi
 # If the PKGS envvar is set, only packages in that list (space delimited) are regenerated.
 generate_docs() {
     GENPKG=""
-    if [ -z "$PKGS" ]; then
+    if [[ -z "$PKGS" ]]; then
         GENPKG=$1
     else
-        for PKG in $PKGS; do
-            if [ "$PKG" == "$1" ]; then
+        for PKG in ${PKGS}; do
+            if [[ "$PKG" == "$1" ]]; then
                 GENPKG=$1
             fi
         done
     fi
 
-    if [ ! -z "$GENPKG" ]; then
+    if [[ ! -z "$GENPKG" ]]; then
         echo -e "\033[0;95m$1\033[0m"
         echo -e "\033[0;93mGenerating typedocs\033[0m"
         pushd ../$2
-        if [ -z "$NOBUILD" ]; then
+        if [[ -z "$NOBUILD" ]]; then
             make ensure && make build && make install
         fi
-        if [ ! -z "$3" ]; then
+        if [[ ! -z "$3" ]]; then
             cd $3
         fi
-        $TOOL_TYPEDOC --json $PULUMI_DOC_TMP/$1.docs.json \
+        ${TOOL_TYPEDOC} --json "${PULUMI_DOC_TMP}/$1.docs.json" \
             --mode modules --includeDeclarations --excludeExternals --excludePrivate
         popd
         echo -e "\033[0;93mGenerating pulumi.io API docs\033[0m"
-        $TOOL_APIDOCGEN $PULUMI_DOC_TMP/$1.docs.json $PULUMI_DOC_BASE/$1
+        ${TOOL_APIDOCGEN} "${PULUMI_DOC_TMP}/$1.docs.json" "${PULUMI_DOC_BASE}/$1"
     fi
 }
 
-generate_docs "pulumi" "pulumi/sdk/nodejs"
 generate_docs "aws" "pulumi-aws" "sdk/nodejs"
 generate_docs "awsx" "pulumi-awsx/nodejs/awsx"
 generate_docs "azure" "pulumi-azure" "sdk/nodejs"
@@ -64,12 +63,13 @@ generate_docs "cloud-azure" "pulumi-cloud/azure"
 generate_docs "cloudflare" "pulumi-cloudflare" "sdk/nodejs"
 generate_docs "docker" "pulumi-docker" "sdk/nodejs"
 generate_docs "eks" "pulumi-eks/nodejs/eks"
-generate_docs "kubernetes" "pulumi-kubernetes" "sdk/nodejs"
-generate_docs "gcp" "pulumi-gcp" "sdk/nodejs"
-generate_docs "openstack" "pulumi-openstack" "sdk/nodejs"
-generate_docs "vsphere" "pulumi-vsphere" "sdk/nodejs"
 generate_docs "f5bigip" "pulumi-f5bigip" "sdk/nodejs"
+generate_docs "gcp" "pulumi-gcp" "sdk/nodejs"
+generate_docs "kubernetes" "pulumi-kubernetes" "sdk/nodejs"
+generate_docs "openstack" "pulumi-openstack" "sdk/nodejs"
 generate_docs "packet" "pulumi-packet" "sdk/nodejs"
+generate_docs "pulumi" "pulumi/sdk/nodejs"
 generate_docs "random" "pulumi-random" "sdk/nodejs"
+generate_docs "vsphere" "pulumi-vsphere" "sdk/nodejs"
 
 echo "Done"
