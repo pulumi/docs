e996869e6a936ea566e927b824ef39a35355edeb
Justin Van Patten <jvp@justinvp.com>
2019-05-23 07:04:47 -0700

    Delete replace_regex.rb
    This was used in one place to include the pulumi CLI command inline inside a markdown file. It's no longer needed, we're doing that in a different way with Hugo.

--- _plugins/replace_regex.rb
+++
@@ -1,6 +0,0 @@
-module ReplaceRegexFilter
-  def replace_regex(input, regex, replacement = '')
-    input.gsub(Regexp.new(regex), replacement.to_s)
-  end
-end
-Liquid::Template.register_filter(ReplaceRegexFilter)
