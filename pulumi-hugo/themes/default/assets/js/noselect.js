bad2ff65e3ee2db7bd48bbebfe434e57504d9b65
Justin Van Patten <jvp@justinvp.com>
2019-07-16 18:03:02 -0700

    Add "copy to clipboard" button to code snippets (#1363)
    * Add "copy to clipboard" button to code snippets
    
    * Update noselect.js to include `language-sh`
    
    * Cleanup tutorials to better support the copy button
    
    * Address feedback
    
    - Use `no-copy` instead of `code-hero`
    - Enable the copy button for snippets that use the code partial (will not be displayed when `chrome` is set to true)
    - Make it look better at small widths
    - Replace `pre { overflow: auto; }` with Tailwind's `overflow-x-auto`
    
    * Just use FontAwesome for the icon
    
    Instead of taking a dependency on the Material Icons, which doesn't have all the icons we need elsewhere, just use an icon from FontAwesome.
    
    Also, add a solid background behind the icon, so when the button is over code (when the browser width is very small), it isn't lost above the text.

--- pulumi-hugo/themes/default/assets/js/noselect.js
+++ pulumi-hugo/themes/default/assets/js/noselect.js
@@ -2,7 +2,7 @@ $(function() {
     // Make "$ " inside bash and shell code snippets unselectable.
     var pattern = /([ |\t]*\$ )/mig;
     var replaceWith = '<span class="no-select">$1</span>';
-    $(".highlight .chroma .language-bash, .highlight .chroma .language-shell").each(function() {
+    $(".highlight .chroma .language-bash, .highlight .chroma .language-shell, .highlight .chroma .language-sh").each(function() {
         $(this).html($(this).html().replace(pattern, replaceWith));
     });
 });
