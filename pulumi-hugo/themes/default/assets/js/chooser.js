0d7a8d3ac323844f12f13866fe57673612de0141
Mike Metral <1112768+metral@users.noreply.github.com>
2019-11-04 15:28:02 -0800

    styling: make TS default k8s-language choice

--- pulumi-hugo/themes/default/assets/js/chooser.js
+++ pulumi-hugo/themes/default/assets/js/chooser.js
@@ -153,5 +153,5 @@ $(function() {
     }
     hideShowChoices("os", selectOs, defaultOsChoice);
     hideShowChoices("cloud", selectCloud, "aws");
-    hideShowChoices("k8s-language", selectK8sLang, "yaml");
+    hideShowChoices("k8s-language", selectK8sLang, "typescript");
 });
