01fade2e4eeb83a23de3198e4e64b666a97205f0
Christian Nunciato <c@nunciato.org>
2019-11-25 14:11:42 -0800

    Support tab selection by query param
    Signed-off-by: Christian Nunciato <c@nunciato.org>

--- assets/js/search.js
+++ assets/js/search.js
@@ -26,20 +26,6 @@
         displaySearchResults([]);
     }
 
-    // Extracts a query string variable from the browser's location.
-    function getQueryVariable(variable) {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split("=");
-
-            if (pair[0] === variable) {
-                return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
-            }
-        }
-    }
-
     // Display the results of the search.
     function displaySearchResults(results) {
         // Hide the spinner.
