83ad6d78e56332d2a4967ca59cacc1c77a729b5b
Christian Nunciato <c@nunciato.org>
2019-11-25 15:00:21 -0800

    Support tab selection by query param (#2044)
    Signed-off-by: Christian Nunciato <c@nunciato.org>

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,20 @@ function generateOnThisPage() {
     }
 }
 
+// Extracts a query string variable from the browser's location.
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+
+        if (pair[0] === variable) {
+            return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
+        }
+    }
+}
+
 (function ($) {
 
     // Set up toggle functionality.
