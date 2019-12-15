d57463b23c4ce9060f9597f77dc7e4e1fd13fe03
Zack Chase <zachary@pulumi.com>
2019-12-15 14:56:42 -0500

    Address feedback; add more comments

--- scripts/markdown_seo_lint.js
+++ scripts/markdown_seo_lint.js
@@ -3,36 +3,70 @@ const yaml = require("js-yaml");
 const markdownlint = require("markdownlint");
 const path = require("path");
 
+/**
+ * REGEX for grabbing the the front matter of a Hugo markdown file. Example:
+ *
+ *     ---
+ *     ...props
+ *     ---
+ */
 const FRONT_MATTER_REGEX = /((^---\s*$[^]*?^---\s*$)|(^\+\+\+\s*$[^]*?^(\+\+\+|\.\.\.)\s*$))(\r\n|\r|\n|$)/m;
 
+/**
+ * Validates if a title exists, has length, and does not have a length over 60 characters.
+ * More info: https://moz.com/learn/seo/title-tag
+ *
+ * @param {string} title The title tag value for a given page.
+ */
 function checkPageTitle(title) {
-    let error = null;
     if (!title) {
-        error = "Missing page title";
-    }else if (title.trim().length === 0) {
-        error = "Page title is empty";
-    }else if (title.trim().length > 60) {
-        error = "Page title exceeds 60 characters";
+        return "Missing page title";
+    }else if (typeof title === "string") {
+        const titleLength = title.trim().length;
+        if (titleLength === 0) {
+            return "Page title is empty";
+        }else if (titleLength > 60) {
+            return "Page title exceeds 60 characters";
+        }
+    }else {
+        return "Page title is not a valid string"
     }
-    return error;
+    return null;
 }
 
+/**
+ * Validates that a meta description exists, has length, is not too short,
+ * and is not too long.
+ * More info: https://moz.com/learn/seo/meta-description
+ *
+ * @param {string} meta The meta description for a given page
+ */
 function checkPageMetaDescription(meta) {
-    let error = null;
     if (!meta) {
-        error = "Missing meta description";
-    }else if (meta.trim().length === 0) {
-        error = "Meta description is empty";
-    }else if (meta.trim().length < 50) {
-        error = "Meta description is too short. Must be at least 50 characters";
-    }else if (meta.trim().length > 160) {
-        error = "Meta descripiton is too long. Must be shorter than 160 characters";
+        return "Missing meta description";
+    }else if (typeof meta === "string") {
+        const metaLength = meta.trim().length;
+        if (metaLength === 0) {
+            return "Meta description is empty";
+        }else if (metaLength < 50) {
+            return "Meta description is too short. Must be at least 50 characters";
+        }else if (metaLength > 160) {
+            return "Meta descripiton is too long. Must be shorter than 160 characters";
+        }
+    }else {
+        return "Meta description is not a valid string";
     }
-    return error;
+    return null;
 }
 
-// Recursively generate a list of markdown files from a given
-// list of paths.
+/**
+ * Builds an array of markdown files to lint and checks each file's front matter
+ * for formatting errors.
+ *
+ * @param {string[]} paths An array of paths to search for markdown files.
+ * @param {Object} [result] The result object returned after finishing searching.
+ * @returns {Object} The markdown file paths to search and an error object for the files front matter.
+ */
 function searchForMarkdown(paths, result) {
     // If the result arg does not exist we should create it.
     if (!result) {
@@ -57,10 +91,10 @@ function searchForMarkdown(paths, result) {
     if (file.indexOf("/content/docs/reference/pkg") > -1) {
         const remaining = paths.slice(1, paths.length);
         return searchForMarkdown(remaining, result);
-
+    }
     // If the path is a directory we want to add the contents of the directory
     // to the list.
-    }else if (isDirectory) {
+    if (isDirectory) {
         const contents = fs.readdirSync(fullPath).map(function(file) {
             return fullPath + "/" + file;
         });
@@ -69,66 +103,131 @@ function searchForMarkdown(paths, result) {
         // Flatten the array.
         const newPaths = [].concat.apply([], paths);
         return searchForMarkdown(newPaths, result);
-
     // Else check if the file suffix is a markdown
     // and add it the resulting file list.
-    }else {
-        if (fileSuffix === "md") {
-            try {
-                // Read the file contents so we can grab the file header.
-                const content = fs.readFileSync(fullPath, "utf8");
+    }
+    if (fileSuffix === "md") {
+        try {
+            // Read the file contents so we can grab the file header.
+            const content = fs.readFileSync(fullPath, "utf8");
 
-                // Grab the file header.
-                const frontMatter = content.match(FRONT_MATTER_REGEX);
+            // Grab the file header.
+            const frontMatter = content.match(FRONT_MATTER_REGEX);
 
-                // Remove the dash blocks around the file header.
-                const fContent = frontMatter[0].split("---").join("");
+            // Remove the dash blocks around the file header.
+            const fContent = frontMatter[0].split("---").join("");
 
-                // Read the yaml.
-                const obj = yaml.load(fContent);
+            // Read the yaml.
+            const obj = yaml.load(fContent);
 
-                if (obj.no_edit_this_page !== true) {
-                    // Build the front matter error object and add the file path.
-                    result.frontMatter[fullPath] = {
-                        error: null,
-                        title: checkPageTitle(obj.title),
-                        metaDescription: checkPageMetaDescription(obj.meta_desc),
-                    };
-                    result.files.push(fullPath);
-                }
-            } catch(e) {
-                // Include the error message in the front matter error object
-                // so we can display it to the user.
+            if (obj.no_edit_this_page !== true) {
+                // Build the front matter error object and add the file path.
                 result.frontMatter[fullPath] = {
-                    error: e.message,
+                    error: null,
+                    title: checkPageTitle(obj.title),
+                    metaDescription: checkPageMetaDescription(obj.meta_desc),
                 };
                 result.files.push(fullPath);
             }
-        }
-
-        // If there are remaining paths in the list, keep going.
-        const remaining = paths.slice(1, paths.length);
-        if (remaining.length > 0) {
-            return searchForMarkdown(remaining, result);
+        } catch(e) {
+            // Include the error message in the front matter error object
+            // so we can display it to the user.
+            result.frontMatter[fullPath] = {
+                error: e.message,
+            };
+            result.files.push(fullPath);
         }
     }
 
+    // If there are remaining paths in the list, keep going.
+    const remaining = paths.slice(1, paths.length);
+    if (remaining.length > 0) {
+        return searchForMarkdown(remaining, result);
+    }
     return result;
 }
 
-// Get a list of markdown files.
-function getMarkdownFileList(parentPath) {
+/**
+ * Builds an array of markdown files to search through from a
+ * given path.
+ *
+ * @param {string} parentPath The path to search for markdown files
+ */
+function getMarkdownFiles(parentPath) {
     const fullParentPath = path.resolve(__dirname, parentPath)
     const dirs = fs.readdirSync(fullParentPath).map(function(dir) {
-        return parentPath + "/" + dir;
+        return path.join(parentPath, dir);
     });
 
     return searchForMarkdown(dirs);
 }
 
-const filesToLint = getMarkdownFileList("../content");
+/**
+ * Groups the result of linting a file for markdown errors.
+ *
+ * @param {Object} result Results of lint errors. See: https://github.com/DavidAnson/markdownlint#usage
+ * @return {Object} An object containing the file path and lint errors.
+ * @return {string} result.path The full path of the linted file.
+ * @return {Object[]} result.errors An array of error objects. Same as the result param.
+ */
+function groupLintErrorOutput(result) {
+    // Grab the keys of the result object.
+    const keys = Object.keys(result);
 
+    // Map over the key array so we can combine front matter errors
+    // with the markdown lint errors.
+    const combinedErrors = keys.map(function(key) {
+        // Get the lint and front matter errors.
+        const lintErrors = result[key];
+        const frontMatterErrors = filesToLint.frontMatter[key];
+
+        // If the front matter error check threw an error add it to the lint
+        // error array. Else add title and meta descriptoins if they exist.
+        if (frontMatterErrors.error) {
+            lintErrors.push({
+                lineNumber: "File Header",
+                ruleDescription: frontMatterErrors.error,
+            });
+        } else {
+            if (frontMatterErrors.title) {
+                lintErrors.push({
+                    lineNumber: "File Header",
+                    ruleDescription: frontMatterErrors.title,
+                });
+            }
+            if (frontMatterErrors.metaDescription) {
+                lintErrors.push({
+                    lineNumber: "File Header",
+                    ruleDescription: frontMatterErrors.metaDescription,
+                });
+            }
+        }
+
+        if (lintErrors.length > 0) {
+            return { path: key, errors: lintErrors };
+        }
+        return null;
+    });
+
+    // Filter out all null values from the combined result array.
+    const filteredErrors = combinedErrors.filter(function(err) {
+        return err !== null;
+    });
+    return filteredErrors;
+}
+
+// Build the lint object for the '../content' directory.
+const filesToLint = getMarkdownFiles("../content");
+
+/**
+ * The config options for lint markdown files. All rules
+ * are enabled by default. The config object let us customize
+ * what rules we enfore and how we enforce them.
+ *
+ * See: https://github.com/DavidAnson/markdownlint
+ */
 const opts = {
+    // The array of markdown files to lint.
     files: filesToLint.files,
     config: {
         // Allow inline HTML.
@@ -154,41 +253,14 @@ const opts = {
     },
 };
 
+// Lint the markdown files.
 const result = markdownlint.sync(opts);
 
-// Get the total number of errors and build a collection
-// of lint errors.
+// Get the total number of errors.
 const errorsCount = [].concat.apply([], Object.values(result)).length;
-const errors = Object.keys(result).map(function(key) {
-    const lintErrors = result[key];
-    const frontMatterErrors = filesToLint.frontMatter[key];
 
-    if (frontMatterErrors.error) {
-        lintErrors.push({
-            lineNumber: "File Header",
-            ruleDescription: frontMatterErrors.error,
-        });
-    }else {
-        if (frontMatterErrors.title) {
-            lintErrors.push({
-                lineNumber: "File Header",
-                ruleDescription: frontMatterErrors.title,
-            });
-        }
-        if (frontMatterErrors.metaDescription) {
-            lintErrors.push({
-                lineNumber: "File Header",
-                ruleDescription: frontMatterErrors.metaDescription,
-            });
-        }
-    }
-
-    if (lintErrors.length > 0) {
-        return { path: key, errors: lintErrors };
-    }else {
-        return null;
-    }
-}).filter(function(item) { return item !== null; });
+// Group the lint errors by file.
+const errors = groupLintErrorOutput(result);
 
 if (errors.length > 0) {
     console.log(`
@@ -211,11 +283,11 @@ ERRORS:
     }
     `);
     process.exit(1);
-}else {
-    console.log(`
+}
+
+console.log(`
 Lint Results:
     - ${filesToLint.length} files parsed.
     - ${errorsCount} errors found.
-    `);
-    process.exit(0);
-}
+`);
+process.exit(0);
