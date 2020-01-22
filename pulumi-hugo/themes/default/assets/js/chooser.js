3434f38a87eebe4af99e70f22114b11264b4ba7b
Joe Duffy <joe@pulumi.com>
2020-01-22 08:31:14 -0800

    Unconditionally choose selections (#2279)
    If a language has been chosen, unconditionally select it. This
    ensures that, even in cases where there are no tabs, we will honor
    the choice and hide/show divs/spans on the page.

--- pulumi-hugo/themes/default/assets/js/chooser.js
+++ pulumi-hugo/themes/default/assets/js/chooser.js
@@ -24,28 +24,26 @@ function selectChoice(kind, choice, extra) {
         }
     });
 
-    // If and only if we found tabs, show and hide divs for the relevant choices.
-    if (choiceTabs > 0) {
-        $("div,span").each(function (i, e) {
-            var classes = getElemClasses(e);
-            for (var i = 0; i < classes.length; i++) {
-                if (classes[i].startsWith(kind + "-prologue-")) {
-                    var next = $(e).next();
-                    if (next) {
-                        if (classes[i] === kind + "-prologue-" + choice) {
-                            $(next).show();
-                        } else {
-                            $(next).hide();
-                        }
+    // Show and hide divs for the relevant choices.
+    $("div,span").each(function (i, e) {
+        var classes = getElemClasses(e);
+        for (var i = 0; i < classes.length; i++) {
+            if (classes[i].startsWith(kind + "-prologue-")) {
+                var next = $(e).next();
+                if (next) {
+                    if (classes[i] === kind + "-prologue-" + choice) {
+                        $(next).show();
+                    } else {
+                        $(next).hide();
                     }
-                    break;
                 }
+                break;
             }
-        });
-
-        if (extra) {
-            extra();
         }
+    });
+
+    if (extra) {
+        extra();
     }
 }
 
@@ -136,17 +134,14 @@ function hideShowChoices(kind, selector, defaultChoice) {
 
     var tabsOnPageKeys = Object.keys(tabsOnPage);
 
-    // If we didn't find any tabs, there's nothing else to do.
-    if (tabsOnPageKeys.length === 0) {
-        return;
-    }
-
     // Now select the right choice based on whether there's a cookie, defaulting as appropriate.
+    // Make sure to select the cookie's choice, even if there are no tabs, as there may still be
+    // divs and spans, like {{% lang X %}} directives, on the page.
     var choiceCookie = decodeURIComponent(
         document.cookie.replace(new RegExp("(?:(?:^|.*;\\s*)pulumi_" + kind + "\\=\\s*([^;]*).*$)|^.*$"), "$1"));
-    if (choiceCookie && tabsOnPage.hasOwnProperty(choiceCookie)) {
+    if (choiceCookie) {
         selector(choiceCookie);
-    } else if (defaultChoice && tabsOnPage.hasOwnProperty(defaultChoice)) {
+    } else if (defaultChoice) {
         selector(defaultChoice);
     } else if (tabsOnPageKeys.length > 0) {
         selector(tabsOnPageKeys[0]);
