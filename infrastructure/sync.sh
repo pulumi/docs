6460ba7aced2eea41cb79ab3fd5223419d7a1bca
Christian Nunciato <c@nunciato.org>
2020-02-07 13:55:41 -0800

    Add some pre-deployment sanity checks (#2406)
    * Add some pre-deployment sanity checks
    
    * Address feedback

--- infrastructure/sync.sh
+++ infrastructure/sync.sh
@@ -6,11 +6,33 @@ site_archive="$1"
 # S3 URL of the destination website bucket (e.g., the one hosting www.pulumi.com).
 site_bucket="$2"
 
+if [ -z "$site_archive" ]; then
+    echo "Missing argument: site_archive"
+    exit 1
+fi
+
+if [ -z "$site_bucket" ]; then
+    echo "Missing argument: site_bucket"
+    exit 1
+fi
+
 # Download the archive from the archive bucket and unpack it into a local folder.
 echo "Downloading $site_archive..."
 aws s3 cp $site_archive .
 mkdir site_contents
 tar -xzvf $(basename "$site_archive") -C site_contents
+
+# Verify index.html exists in the root.
+if [ ! -f "site_contents/index.html" ]; then
+    echo "Missing root index.html. See the archive extraction log above for details."
+    exit 1
+fi
+
+# Verify we have at least 1000 index.html files in total across the site.
+if [ ! "$(find site_contents -type f | grep index.html | wc -l)" -ge 1000 ]; then
+    echo "Page-count check failed. See the archive extraction log above for details."
+    exit 1
+fi
 
 # Upload the JS and CSS bundles first.
 cd site_contents
