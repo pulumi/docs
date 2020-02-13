d4e1b0660ddb5dd52fdf1e457f3714e718ead21f
Zack Chase <zachary@pulumi.com>
2020-02-13 11:38:39 -0800

    UX improvements to events pages (#2419)
    * Move filter and CTA to right side of event listing page
    
    * Add heading to event list showing number of events available when filtering
    
    * Remove calendar embed and use button to bring up calendar modal
    
    * Slightly prettify the event-listings page
    
    * Slightly prettify the event-detail page
    
    * Make the registration button orange
    
    * Add ability to use filter query param to pre filter events
    
    * Address feedback
    
    * Remove excess console.log
    
    * Update assets/js/event-filtering.js
    
    Co-Authored-By: Justin Van Patten <jvp@justinvp.com>
    
    Co-authored-by: Christian Nunciato <christian@pulumi.com>
    Co-authored-by: Justin Van Patten <jvp@justinvp.com>

--- assets/js/event-filtering.js
+++ assets/js/event-filtering.js
@@ -1,19 +1,4 @@
 $(function() {
-    var eventsElements = $(".event-tags span");
-    var eventFilterParent = $("#eventFilter");
-
-    // Check to see if the event elements exists.
-    if (!eventsElements.length) {
-        // Stop the script as there are no tags to filter.
-        return;
-    }
-    if (!eventFilterParent.length) {
-        // Stop the script because the event filter element does not exist.
-        return;
-    }
-
-    var tags = [];
-
     /**
      * This function creates a checkbox from a given text string.
      * It will set the label and the value to whatever the input string is.
@@ -21,17 +6,31 @@ $(function() {
      * @param {string} text The label and value of the checkbox
      */
     function createCheckbox(text) {
+        var filterValue = getQueryVariable("filter");
+
         var container = document.createElement("div");
-        container.className = "my-2";
+        container.className = "my-2 uppercase flex items-center";
 
         var checkbox = document.createElement("input");
         checkbox.id = "checkbox-" + text;
         checkbox.type = "checkbox";
-        checkbox.className = "mx-2 cursor-pointer";
-        checkbox.value = text;
+        checkbox.className = "mr-2 cursor-pointer";
+        checkbox.value = text.toLowerCase();
+
+        // If the filter query parameter is available use it to determine
+        // what checkboxes should initially be checked. If the query param
+        // is not set, check all the boxes.
+        if (filterValue !== undefined) {
+            var shouldBeChecked = filterValue.toLowerCase().split(",").indexOf(checkbox.value) > -1;
+            if (shouldBeChecked) {
+                checkbox.setAttribute("checked", true);
+            }
+        } else {
+            checkbox.setAttribute("checked", true);
+        }
 
         var label = document.createElement("label");
-        label.innerText = text;
+        label.innerText = text + "s";
         label.className = "cursor-pointer";
         label.setAttribute("for", checkbox.id);
 
@@ -41,27 +40,9 @@ $(function() {
         return container;
     }
 
-    // Loop through the tags and create a unique array of tag names and
-    // append a checkbox to the event filter for each unique tag.
-    for (var i = 0; i < eventsElements.length; i++) {
-        var elem = eventsElements[i];
-
-        // Grab the text of the element. We use the .text method because
-        // it will grab the string value of the text.
-        //
-        // See: https://api.jquery.com/text/
-        var text = $(elem).text();
-
-        if (tags.indexOf(text) === -1) {
-            tags.push(text);
-            const input = createCheckbox(text);
-            eventFilterParent.append(input);
-        }
-    }
-
-    // This click handler will determine which checkboxes are selected
-    // and then provide them to the filter event function to filter the events.
-    $("#eventFilter input[type='checkbox']").on("click", function() {
+    // This function grabs the filter checkboxes, loops through them to
+    // determine what is checked, and then filters the events.
+    function getFilterValuesAndFilterList() {
         var inputs = $("input[type='checkbox']");
         var chosenInputs = [];
 
@@ -69,12 +50,12 @@ $(function() {
             var input = inputs[i];
             var isChecked = $(input).prop("checked");
             if (isChecked) {
-                chosenInputs.push($(input).val().toLowerCase());
+                chosenInputs.push($(input).val());
             }
         }
 
         filterEventList(chosenInputs);
-    });
+    }
 
     /**
      * This function checks to see if two arrays have any value in common. This
@@ -102,7 +83,8 @@ $(function() {
      * @param {string[]} tags An array of tags that should appear in the event list.
      */
     function filterEventList(tags) {
-        var events = $("#eventList li");
+        var events = $("#event-list li");
+        var visibleEvents = 0;
 
         for (var i = 0; i < events.length; i++) {
             var event = $(events[i]);
@@ -110,10 +92,60 @@ $(function() {
             var shouldBeVisible = tags.length === 0 ? true : checkForIntersection(tags, dataEventType);
 
             if (shouldBeVisible) {
+                visibleEvents += 1;
                 event.removeClass("hidden");
             } else {
                 event.addClass("hidden");
             }
         }
+        if (visibleEvents === events.length) {
+            $("#event-list-heading").text("All Upcoming Events")
+        } else {
+            $("#event-list-heading").text(visibleEvents + " Upcoming Events");
+        }
     }
+
+    // This is the start of the code that is executed on page load.
+    var eventsElements = $(".event-tags span");
+    var eventFilterParent = $("#eventFilter");
+
+    // Check to see if the event elements exists.
+    if (!eventsElements.length) {
+        // Stop the script as there are no tags to filter.
+        return;
+    }
+    if (!eventFilterParent.length) {
+        // Stop the script because the event filter element does not exist.
+        return;
+    }
+
+    var tags = [];
+
+    // Loop through the tags and create a unique array of tag names and
+    // append a checkbox to the event filter for each unique tag.
+    for (var i = 0; i < eventsElements.length; i++) {
+        var elem = eventsElements[i];
+
+        // Grab the text of the element. We use the .text method because
+        // it will grab the string value of the text.
+        //
+        // See: https://api.jquery.com/text/
+        var text = $(elem).text();
+
+        if (tags.indexOf(text) === -1) {
+            tags.push(text);
+            const input = createCheckbox(text);
+            eventFilterParent.append(input);
+        }
+
+    }
+
+    // Filter the events based on the filter query parameter.
+    getFilterValuesAndFilterList();
+
+    // This click handler will determine which checkboxes are selected
+    // and then provide them to the filter event function to filter the events.
+    $("#eventFilter input[type='checkbox']").click(function() {
+        getFilterValuesAndFilterList();
+    });
 });
--- content/events/devopsdays-madrid-2020-02-21/index.md
+++ content/events/devopsdays-madrid-2020-02-21/index.md
@@ -1,6 +1,6 @@
 ---
 # Name of the event.
-title: "DevOpsDays Madrid"
+title: "Devopsdays Madrid"
 
 # Events with external registrations should not be indexed
 # and have redirect to the external registration page.
