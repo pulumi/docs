59479341cf610000a4a3e6cba775440a031e441e
Zack Chase <zachary@pulumi.com>
2020-02-20 10:08:27 -0800

    Add target selector to purgecss whitelist (#2489)

--- pulumi-hugo/themes/default/assets/config/postcss.config.js
+++ pulumi-hugo/themes/default/assets/config/postcss.config.js
@@ -37,7 +37,7 @@ module.exports = {
             ],
 
             // Whitelist specific classes that were being removed.
-            whitelist: ["supported-cicd-platforms", ":not", "md:max-w-lg", "blink", "typing", "char"],
+            whitelist: ["supported-cicd-platforms", ":not", ":target", "md:max-w-lg", "blink", "typing", "char"],
 
             // Whitelist custom parent selectors and their children.
             whitelistPatterns: [/^fa-/, /^hs-/, /^highlight$/, /^pagination$/, /^code-/, /^copy-/, /^carousel/],
