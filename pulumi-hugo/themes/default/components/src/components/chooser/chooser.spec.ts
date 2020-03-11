570f98474510580dec4d20e8fc5ce1291f39398a
Christian Nunciato <c@nunciato.org>
2020-03-11 10:10:05 -0700

    Add Stencil.js for web components (#2626)
    * Add Stencil.js for web components
    * Address feedback

---
+++ pulumi-hugo/themes/default/components/src/components/chooser/chooser.spec.ts
@@ -0,0 +1,8 @@
+import { Chooser } from './chooser';
+
+describe("pulumi-chooser", () => {
+
+    it("builds", () => {
+        expect(new Chooser()).toBeTruthy();
+    });
+});
---
+++ pulumi-hugo/themes/default/components/src/store/actions/preferences.ts
@@ -0,0 +1,58 @@
+import { TypeKeys } from "./index";
+import { LanguageKey, K8sLanguageKey, OSKey, CloudKey } from "../../components/chooser/chooser";
+
+export interface SetLanguage {
+    type: TypeKeys.SET_LANGUAGE;
+    key: LanguageKey;
+}
+
+export interface SetK8sLanguage {
+    type: TypeKeys.SET_K8S_LANGUAGE;
+    key: K8sLanguageKey;
+}
+
+export interface SetOS {
+    type: TypeKeys.SET_OS;
+    key: OSKey;
+}
+
+export interface SetCloud {
+    type: TypeKeys.SET_CLOUD;
+    key: CloudKey;
+}
+
+// Set the currently selected language.
+export const setLanguage = (key: LanguageKey) => (dispatch, _getState) => {
+    const action: SetLanguage = {
+        type: TypeKeys.SET_LANGUAGE,
+        key: key,
+    };
+    dispatch(action);
+};
+
+// Set the currently Kubernetes language.
+export const setK8sLanguage = (key: K8sLanguageKey) => (dispatch, _getState) => {
+    const action: SetK8sLanguage = {
+        type: TypeKeys.SET_K8S_LANGUAGE,
+        key: key,
+    };
+    dispatch(action);
+};
+
+// Set the currently OS.
+export const setOS = (key: OSKey) => (dispatch, _getState) => {
+    const action: SetOS = {
+        type: TypeKeys.SET_OS,
+        key: key,
+    };
+    dispatch(action);
+};
+
+// Set the currently selected cloud.
+export const setCloud = (key: CloudKey) => (dispatch, _getState) => {
+    const action: SetCloud = {
+        type: TypeKeys.SET_CLOUD,
+        key: key,
+    };
+    dispatch(action);
+};
