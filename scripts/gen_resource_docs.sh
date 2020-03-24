80aeeac3c771cc31a59bc78c746f875318a3f7c2
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-03-24 10:37:46 -0700

    Update the update_repos.sh script to accept optional repo override.

--- scripts/gen_resource_docs.sh
+++ scripts/gen_resource_docs.sh
@@ -5,7 +5,17 @@ PACKDIR="./content/docs/reference/pkg"
 ABSOLUTEPACKDIR="$(pwd)/content/docs/reference/pkg"
 TOOL_APIDOCGEN="go run ./tools/resourcedocsgen/*.go"
 
-./scripts/update_repos.sh
+TOOLS_REPOS=(
+    "pulumi-aws"
+    "pulumi-azure"
+    "pulumi-azuread"
+    "pulumi-gcp"
+)
+
+for repo in "${TOOLS_REPOS[@]}"
+do
+    ./scripts/update_repos.sh ${repo}
+done
 
 echo "Generating docs templates bundle in pulumi/pulumi"
 pushd ../pulumi
@@ -16,7 +26,7 @@ for PROVIDER in "aws" "azure" "azuread" "gcp" ; do \
     echo "Removing the ${PACKDIR}/${PROVIDER} dir..."
     rm -rf "${PACKDIR}/${PROVIDER}"
 
-    echo "Running docs generator from schema..."
+    echo "Running docs generator from schema for ${PROVIDER}..."
     ${TOOL_APIDOCGEN} ${ABSOLUTEPACKDIR}/${PROVIDER} ../pulumi-${PROVIDER}/cmd/pulumi-resource-${PROVIDER}/schema.json || exit 3
 
     echo "Done generating resource docs for ${PROVIDER}"
--- scripts/update_repos.sh
+++ scripts/update_repos.sh
@@ -3,53 +3,55 @@
 
 set -o nounset -o errexit -o pipefail
 
+REPO_OVERRIDE=${1:-}
+
 TOOLS_REPOS=(
-    # "pulumi"
-    # "pulumi-aiven"
-    # "pulumi-alicloud"
+    "pulumi"
+    "pulumi-aiven"
+    "pulumi-alicloud"
     "pulumi-aws"
-    # "pulumi-awsx"
+    "pulumi-awsx"
     "pulumi-azure"
     "pulumi-azuread"
-    # "pulumi-cloud"
-    # "pulumi-cloudamqp"
-    # "pulumi-cloudflare"
-    # "pulumi-consul"
-    # "pulumi-datadog"
-    # "pulumi-digitalocean"
-    # "pulumi-dnsimple"
-    # "pulumi-docker"
-    # "pulumi-eks"
-    # "pulumi-fastly"
-    # "pulumi-f5bigip"
+    "pulumi-cloud"
+    "pulumi-cloudamqp"
+    "pulumi-cloudflare"
+    "pulumi-consul"
+    "pulumi-datadog"
+    "pulumi-digitalocean"
+    "pulumi-dnsimple"
+    "pulumi-docker"
+    "pulumi-eks"
+    "pulumi-fastly"
+    "pulumi-f5bigip"
     "pulumi-gcp"
-    # "pulumi-gitlab"
-    # "pulumi-kafka"
-    # "pulumi-keycloak"
-    # "pulumi-kubernetes"
-    # "pulumi-kubernetesx"
-    # "pulumi-linode"
-    # "pulumi-mailgun"
-    # "pulumi-mysql"
-    # "pulumi-newrelic"
-    # "pulumi-okta"
-    # "pulumi-openstack"
-    # "pulumi-packet"
-    # "pulumi-policy"
-    # "pulumi-postgresql"
-    # "pulumi-rabbitmq"
-    # "pulumi-rancher2"
-    # "pulumi-random"
-    # "pulumi-signalfx"
-    # "pulumi-spotinst"
-    # "pulumi-terraform"
-    # "pulumi-tls"
-    # "pulumi-vault"
-    # "pulumi-vsphere"
+    "pulumi-gitlab"
+    "pulumi-kafka"
+    "pulumi-keycloak"
+    "pulumi-kubernetes"
+    "pulumi-kubernetesx"
+    "pulumi-linode"
+    "pulumi-mailgun"
+    "pulumi-mysql"
+    "pulumi-newrelic"
+    "pulumi-okta"
+    "pulumi-openstack"
+    "pulumi-packet"
+    "pulumi-policy"
+    "pulumi-postgresql"
+    "pulumi-rabbitmq"
+    "pulumi-rancher2"
+    "pulumi-random"
+    "pulumi-signalfx"
+    "pulumi-spotinst"
+    "pulumi-terraform"
+    "pulumi-tls"
+    "pulumi-vault"
+    "pulumi-vsphere"
 )
 
-for repo in "${TOOLS_REPOS[@]}"
-do
+update_repo() {
+    repo=$1
     echo -e "\033[0;95m--- Updating repo pulumi/${repo} ---\033[0m"
     pushd . >/dev/null 2>&1
     cd "../${repo}"
@@ -62,4 +64,13 @@ do
     git -c advice.detachedHead=false checkout $LATEST_RELEASE >/dev/null
     echo -e "\033[0;96m$LATEST_RELEASE\033[0m"
     popd >/dev/null 2>&1
-done
+}
+
+if [ -z "${REPO_OVERRIDE:-}" ]; then
+    for repo in "${TOOLS_REPOS[@]}"
+    do
+        update_repo ${repo}
+    done
+else
+    update_repo ${REPO_OVERRIDE}
+fi
