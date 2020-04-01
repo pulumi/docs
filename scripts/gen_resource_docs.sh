24037853eebd8518e5f3dde626b2a257a94f4251
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-04-01 13:56:25 -0700

    Allow passing a provider name to the gen_resource_docs for CI purposes.

--- scripts/gen_resource_docs.sh
+++ scripts/gen_resource_docs.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# A script that facilitates generating resource docs using the tools/resourcedocsgen Go-based
+# tool. This script optionally allows passing the provider name (without the "pulumi-" prefix.)
+
 set -o nounset -o errexit -o pipefail
+
+REPO_OVERRIDE=${1:-}
 
 PACKDIR="./content/docs/reference/pkg"
 ABSOLUTEPACKDIR="$(pwd)/content/docs/reference/pkg"
-TOOL_RESDOCGEN="go run ./tools/resourcedocsgen/*.go -v=3 -logtostderr"
+TOOL_RESDOCGEN="go run ./tools/resourcedocsgen/*.go -logtostderr"
 
 PROVIDERS=(
-    "kubernetes"
+    "aws"
+    "azure"
+    "azuread"
+    "gcp"
 )
 
 echo "Generating docs templates bundle in pulumi/pulumi"
@@ -14,28 +22,37 @@ pushd ../pulumi
 make generate
 popd
 
-for PROVIDER in "${PROVIDERS[@]}" ; do \
-    # ./scripts/update_repos.sh "pulumi-${PROVIDER}"
+generate_docs() {
+    provider=$1
+    ./scripts/update_repos.sh "pulumi-${provider}"
 
-    echo "Removing the ${PACKDIR}/${PROVIDER} dir..."
-    rm -rf "${PACKDIR}/${PROVIDER}"
+    echo "Removing the ${PACKDIR}/${provider} dir..."
+    rm -rf "${PACKDIR}/${provider}"
 
-    TFGEN=pulumi-tfgen-${PROVIDER}
+    TFGEN=pulumi-tfgen-${provider}
 
-    #pushd ../pulumi-${PROVIDER}
-    #echo "Running pulumi-tfgen-${TFGEN} to generate provider schema..."
-    #make generate_schema
-    #popd
+    pushd ../pulumi-${provider}
+    echo "Running pulumi-tfgen-${TFGEN} to generate provider schema..."
+    make generate_schema
+    popd
 
-    if [ $PROVIDER = "kubernetes" ]; then
-        SCHEMA_FILE="../pulumi-${PROVIDER}/sdk/schema/schema.json"
+    if [ $provider = "kubernetes" ]; then
+        SCHEMA_FILE="../pulumi-${provider}/sdk/schema/schema.json"
     else
-        SCHEMA_FILE="../pulumi-${PROVIDER}/provider/cmd/pulumi-resource-${PROVIDER}/schema.json"
+        SCHEMA_FILE="../pulumi-${provider}/provider/cmd/pulumi-resource-${provider}/schema.json"
     fi
 
-    echo "Running docs generator from schema for ${PROVIDER}..."
-    ${TOOL_RESDOCGEN} ${ABSOLUTEPACKDIR}/${PROVIDER} ${SCHEMA_FILE} || exit 3
+    echo "Running docs generator from schema for ${provider}..."
+    ${TOOL_RESDOCGEN} ${ABSOLUTEPACKDIR}/${provider} ${SCHEMA_FILE} || exit 3
 
-    echo "Done generating resource docs for ${PROVIDER}"
+    echo "Done generating resource docs for ${provider}"
     echo ""
-done
+}
+
+if [ -z "${REPO_OVERRIDE:-}" ]; then
+    for PROVIDER in "${PROVIDERS[@]}" ; do \
+        generate_docs $PROVIDER
+    done
+else
+    generate_docs ${REPO_OVERRIDE}
+fi
