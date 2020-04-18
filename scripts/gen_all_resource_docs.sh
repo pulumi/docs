1dc032b5bf617e64fe9ece7a70c5055b80d8708b
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-04-18 13:55:52 -0700

    Make committing to git optional.

--- scripts/gen_all_resource_docs.sh
+++ scripts/gen_all_resource_docs.sh
@@ -3,6 +3,10 @@
 # A script to generate the schema-based resource docs for all of the providers for which we can generate
 # a Pulumi schema.
 
+# Pass any value to the script as the first to indicate that you
+# also this script to commit each provider's docs changes automatically.
+GIT_COMMIT=${1:-}
+
 # Adding a new repo to this list? Ensure that the repo's `Makefile` has a target called
 # `generate_schema`.
 REPOS=(
@@ -44,27 +48,31 @@ REPOS=(
 )
 
 branch=$(git rev-parse --abbrev-ref HEAD)
-if [ $branch = "master" ]; then
+if [ "$branch" = "master" ]; then
     echo "Cannot generate docs while in the master branch. Please create a new branch and then try again."
     exit 1
 fi
 
 for REPO in "${REPOS[@]}" ; do \
-    ./scripts/gen_resource_docs.sh $REPO true
+    ./scripts/gen_resource_docs.sh "$REPO" true
 
-    git add .
-    git commit -am "Generate resource docs for pulumi-${REPO}"
+    if [ -n "${GIT_COMMIT:-}" ]; then
+      git add .
+      git commit -am "Generate resource docs for pulumi-${REPO}"
+    fi
 done
 
-while true; do
-    read -p "Push commits to origin?" yn
-    case $yn in
-        [Yy]* )
-            echo "Pushing ${branch} to origin..."
-            git push origin ${branch}
-            break
-            ;;
-        [Nn]* ) echo "Exiting. You said No."; exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+if [ -n "${GIT_COMMIT:-}" ]; then
+  while true; do
+      read -r -p "Push commits to origin?" yn
+      case $yn in
+          [Yy]* )
+              echo "Pushing ${branch} to origin..."
+              git push origin "${branch}"
+              break
+              ;;
+          [Nn]* ) echo "Exiting. You said No."; exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+  done
+fi
