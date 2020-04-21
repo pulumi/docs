d9d18f099ad4ac5fc202b1594660cf7c9b4bf129
Zack Chase <zachary@pulumi.com>
2020-04-21 10:18:20 -0700

    Add UTM param tracking to form submissions (#2955)
    * Add UTM param tracking to form submissions
    
    * Address feedback

--- assets/js/webinar.js
+++ assets/js/webinar.js
@@ -8,6 +8,32 @@ function localizeDateTime(date) {
     return localeDate.toLocaleString(undefined, options);
 }
 
+/**
+ * This function parses the cookie string and returns an object of the current cookies.
+ */
+function parseCookie() {
+    return document.cookie.split(";").reduce(function(obj, cookie) {
+        var index = cookie.indexOf("=");
+        var key = cookie.substring(0, index).trim();
+        var value = cookie.substring(index + 1).trim();
+        obj[key] = value;
+        return obj;
+    }, {});
+}
+
+/**
+ * This function parses the UTM cookie and returns and object of the cookie.
+ *
+ * @param {string} utmCookieString The value of the '__utmzz' cookie. Values are separated by '|'.
+ */
+function parseUTMCookie(utmCookieString) {
+    return (utmCookieString || "").split("|").reduce(function(obj, utm) {
+        var parts = utm.split("=");
+        obj[parts[0]] = parts[1];
+        return obj;
+    }, {});
+}
+
 $(function() {
     var webinarListings = $("#webinarListings")[0];
     var webinarDetails = $("#webinarLandingPage")[0];
@@ -43,6 +69,37 @@ $(function() {
                 $("#" + listId).removeClass("hidden");
                 $(this).removeClass("text-gray-500").removeClass("hover:text-blue-800").addClass("text-blue-700");
             });
+        });
+    }
+
+    // Check that the analytics track function is available.
+    var analyticsAvailable = window.analytics && window.analytics.track &&
+                             (typeof window.analytics.track === "function");
+
+    if (webinarDetails && analyticsAvailable) {
+        // Parse the cookie and grab the UTM cookie values.
+        var cookies = parseCookie();
+        var utmCookie = parseUTMCookie(cookies["__utmzz"]);
+
+        // Listen for the HubSpot form to be loaded.
+        window.addEventListener('message', event => {
+            if(event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormReady') {
+                var form = $(".hbspt-form form");
+                console.log(form);
+
+                // Send an analytics event with the UTM values when the form is submitted.
+                form.submit(function() {
+                    var submissionData = {
+                        formId: form.attr("data-form-id"),
+                        email: $(".hbspt-form form input[name='email']").val(),
+                        utmCampaign: utmCookie.utmccn || "(not set)",
+                        utmSource: utmCookie.utmcsr || "(direct)",
+                        utmMedium: utmCookie.utmcmd || "(none)",
+                    };
+
+                    window.analytics.track("form-submission", submissionData);
+                });
+            }
         });
     }
 });
