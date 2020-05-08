9227f652818196ba1941a767c338969efc9f3c7b
Sean Holung <sholung@pulumi.com>
2020-05-08 12:08:03 -0700

    Fix language selection logic
    Fix language selection logic

--- components/src/util/util.ts
+++ components/src/util/util.ts
@@ -4,12 +4,12 @@ export function getQueryVariable(paramKey) :string {
     const query = window.location.search.substring(1);
     const vars = query.split("&");
 
+    let paramVal = null
     vars.forEach( v => {
         const pair = v.split("=");
         if (pair[0] === paramKey) {
-            return decodeURIComponent(pair[1].replace(/\+/g, "%20"));
+            paramVal = decodeURIComponent(pair[1].replace(/\+/g, "%20"));
         }
     })
-
-    return null;
+    return paramVal;
 }
