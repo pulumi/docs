9196f6a74070aab4e6f318be7a2512f416eb3ba7
Christian Nunciato <c@nunciato.org>
2020-07-01 16:24:48 -0700

    Compile CSS and JS separately from Hugo

--- scripts/run-hugo-build.sh
+++ scripts/run-hugo-build.sh
@@ -1,13 +1,29 @@
-# Start a heartbeat process to write data to STDOUT even if there is a long
-# delay in updates from the Hugo build process. This keeps Travis from assuming our
-# jobs have hung. (Ping every min, stop beating after an hour.)
-./scripts/emit-heartbeat.sh 60 3600 &
-HEARTBEAT_PID=$!
-echo "Started hearbeat process on PID ${HEARTBEAT_PID}"
+#!/bin/bash
 
-stop_heartbeat() {
-    kill -9 ${HEARTBEAT_PID} || echo "Error killing heartbeat process."
-}
-trap stop_heartbeat EXIT
+set -o errexit -o pipefail
 
-NODE_ENV=production hugo --minify
+export NODE_ENV=production
+
+# In most cases, we'll get the SHA from GitHub Actions, but in case we don't,
+# just fall back to Git.
+export GIT_SHA=${GITHUB_SHA:=$(git rev-list HEAD | head -1)}
+
+# Paths to the CSS and JS bundles we'll generate below. Note that environment variables
+# are read by some templates during the Hugo build process.
+export CSS_BUNDLE="public/css/styles.${GIT_SHA}.css"
+export JS_BUNDLE="public/js/bundle.min.${GIT_SHA}.js"
+
+# Run the Hugo build.
+printf "Running Hugo...\n\n"
+hugo --minify --cleanDestinationDir --templateMetrics --templateMetricsHints
+
+printf "\nCompiling the JavaScripts...\n\n"
+npx tsc --outFile ${JS_BUNDLE}
+
+printf "Compiling the Sass...\n\n"
+npx node-sass assets/sass/styles.scss --output-style compressed > ${CSS_BUNDLE}
+
+printf "Running PostCSS...\n\n"
+npx postcss-cli ${CSS_BUNDLE} --config assets/config --replace
+
+printf "Done!\n\n"
