42c4986451a3b41bec1900ef90ef8cbae82132a5
Christian Nunciato <c@nunciato.org>
2020-08-04 14:16:05 -0700

    Handle missing credentials

--- scripts/ci-pull-request.sh
+++ scripts/ci-pull-request.sh
@@ -2,6 +2,15 @@
 
 set -o errexit -o pipefail
 
+# See if we have the requisite credentials. If not, we might be in a fork. In that case,
+# run a PR build, but skip all the deployment stuff.
+if [ -z "${AWS_ACCESS_KEY_ID:-}" ] || [ -z "${AWS_SECRET_ACCESS_KEY:-}" ] || [ -z "${PULUMI_ACCESS_TOKEN:-}" ]; then
+    echo "Missing secret tokens, possibly due to a forked PR."
+    echo "Running a build, but will skip the sync to S3 and Pulumi preview."
+    ./scripts/build-site.sh preview
+    exit
+fi
+
 source ./scripts/ci-login.sh
 
 ./scripts/build-site.sh preview
