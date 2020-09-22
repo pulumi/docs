0ae01c4084d7564e51df996d06d5c011df668398
Christian Nunciato <c@nunciato.org>
2020-09-22 11:04:12 -0700

    Extend the session duration to 2h

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -3,7 +3,7 @@
 set -o errexit -o pipefail
 
 echo "Assuming the production CI role..."
-eval $(assume-role "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
+eval $(assume-role -duration "2h0m0s" "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
 
 echo "Selecting the pulumi/production stack"
 pulumi login
