4b24ed3fde98473f71242c1f12316d8432077f91
Christian Nunciato <c@nunciato.org>
2020-10-07 15:31:39 -0700

    Whitelist BambooHR selectors

--- assets/config/postcss.config.js
+++ assets/config/postcss.config.js
@@ -64,7 +64,7 @@ module.exports = {
             whitelist: ["supported-cicd-platforms", ":not", ":target", "md:max-w-lg", "blink", "typing", "char", "resource-deprecated", "btn-scroll-top"],
 
             // Whitelist custom parent selectors and their children.
-            whitelistPatterns: [/^fa-/, /^hs-/, /^highlight$/, /^pagination$/, /^code-/, /^copy-/, /^carousel/, /^bg-/],
+            whitelistPatterns: [/^fa-/, /^hs-/, /^highlight$/, /^pagination$/, /^code-/, /^copy-/, /^carousel/, /^bg-/, /BambooHR-/],
             whitelistPatternsChildren: [
                 /^hs-/,
                 /^highlight$/,
