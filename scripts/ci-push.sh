a8afb6c02a33a0885892e8637e15ff9069b032e1
Christian Nunciato <c@nunciato.org>
2020-10-28 08:07:53 -0700

    Queue before deploy to allow concurrent builds (#4485)

--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -2,13 +2,14 @@
 
 set -o errexit -o pipefail
 
-# Wait for in-progress jobs to complete before proceeding.
-node ./scripts/await-in-progress.js
-
 source ./scripts/ci-login.sh
 
 ./scripts/build-site.sh
 ./scripts/sync-and-test-bucket.sh
 ./scripts/publish-sentry-release.sh
+
+# Wait for in-progress jobs to complete before proceeding.
+node ./scripts/await-in-progress.js
+
 ./scripts/run-pulumi.sh update
 ./scripts/make-s3-redirects.sh
