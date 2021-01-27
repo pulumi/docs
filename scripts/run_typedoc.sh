213c4dbc186f63c7db2c0ae9fd2325fda1a1d3e6
Christian Nunciato <c@nunciato.org>
2021-01-27 08:49:45 -0800

    Remove and redirect nodejs/civo

--- scripts/run_typedoc.sh
+++ scripts/run_typedoc.sh
@@ -78,7 +78,6 @@ generate_docs() {
 REPOS=(
     "awsx,pulumi-awsx/nodejs/awsx"
     "azure-nextgen,pulumi-azure-nextgen,sdk/nodejs"
-    "civo,pulumi-civo,sdk/nodejs"
     "cloud,pulumi-cloud/api"
     "cloudamqp,pulumi-cloudamqp,sdk/nodejs"
     "cloudflare,pulumi-cloudflare,sdk/nodejs"
