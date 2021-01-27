ee3e0035ed0fa096277b5645deff5798b3bd78c1
Christian Nunciato <c@nunciato.org>
2021-01-27 08:46:44 -0800

    Remove and redirect nodejs/azure

--- scripts/run_typedoc.sh
+++ scripts/run_typedoc.sh
@@ -77,7 +77,6 @@ generate_docs() {
 
 REPOS=(
     "awsx,pulumi-awsx/nodejs/awsx"
-    "azure,pulumi-azure,sdk/nodejs"
     "azure-nextgen,pulumi-azure-nextgen,sdk/nodejs"
     "azuread,pulumi-azuread,sdk/nodejs"
     "azuredevops,pulumi-azuredevops,sdk/nodejs"
