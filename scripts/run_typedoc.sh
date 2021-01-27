99a5b6c86a63e6b40f28e77bd403b88127fb75ee
Christian Nunciato <c@nunciato.org>
2021-01-27 08:51:03 -0800

    Remove and redirect nodejs/cloudflare

--- scripts/run_typedoc.sh
+++ scripts/run_typedoc.sh
@@ -79,7 +79,6 @@ REPOS=(
     "awsx,pulumi-awsx/nodejs/awsx"
     "azure-nextgen,pulumi-azure-nextgen,sdk/nodejs"
     "cloud,pulumi-cloud/api"
-    "cloudflare,pulumi-cloudflare,sdk/nodejs"
     "cloudinit,pulumi-cloudinit,sdk/nodejs"
     "consul,pulumi-consul,sdk/nodejs"
     "datadog,pulumi-datadog,sdk/nodejs"
