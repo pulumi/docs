1fbd6c693a49520b6ca24ea97dcc99d2d8434818
Christian Nunciato <c@nunciato.org>
2021-02-03 12:59:15 -0800

    Refactor HubSpot forms component (#5114)
    * Refactor HubSpot forms component
    
    * Load the HS forms script conditionally/dynamically

--- pulumi-hugo/themes/default/components/src/components/hubspot-form/hubspot-form.e2e.ts
+++ pulumi-hugo/themes/default/components/src/components/hubspot-form/hubspot-form.e2e.ts
@@ -1,11 +1,11 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage } from "@stencil/core/testing";
 
-describe('pulumi-hubspot-form', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<pulumi-hubspot-form></pulumi-hubspot-form>');
+describe("pulumi-hubspot-form", () => {
+    it("renders", async () => {
+        const page = await newE2EPage();
+        await page.setContent(`<pulumi-hubspot-form form-id="12345"></pulumi-hubspot-form>`);
 
-    const element = await page.find('pulumi-hubspot-form');
-    expect(element).toHaveClass('hydrated');
-  });
+        const element = await page.find("pulumi-hubspot-form");
+        expect(element).toHaveClass("hydrated");
+    });
 });
