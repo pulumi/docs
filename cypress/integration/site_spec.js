dd3edbdb772a3adb3a1652bb4c412af435d0f496
Christian Nunciato <c@nunciato.org>
2021-04-14 16:09:53 -0700

    Incorporate pulumi-hugo-internal

--- cypress/integration/site_spec.js
+++ cypress/integration/site_spec.js
@@ -9,18 +9,21 @@ describe("www.pulumi.com", () => {
         it("loads and applies CSS", () => {
             // Checking the computed background-color value validates that the CSS bundle
             // was properly loaded and applied.
-            cy.get(".nav-main")
-                .invoke("css", "background-color")
-                .should("equal", "rgb(55, 26, 71)");
+            cy.get(".header-container")
+                .invoke("css", "position")
+                .should("equal", "sticky");
         });
 
         it("loads and applies JavaScript", () => {
             // Checking the carousel validates that the JS bundle was loaded and applied
             // (excluding Stencil components, which are bundled separately).
-            cy.contains("Manage").click();
-            cy.get(".carousel-item-description:last")
-                .invoke("css", "opacity")
-                .should("equal", "1");
+            cy.get(".header-container")
+                .should("not.have.class", "is-pinned");
+
+            cy.scrollTo(0, 250);
+
+            cy.get(".header-container")
+                .should("have.class", "is-pinned");
         });
     });
 
