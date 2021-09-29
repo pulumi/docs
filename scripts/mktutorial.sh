9948912ae8172f4c2ed087d5ba67e51f02384848
Devon Grove <devon@pulumi.com>
2021-09-29 11:10:01 -0700

    Updated mktutorial.sh path and moved tutorials to registry hierarchy

--- scripts/mktutorial.sh
+++ scripts/mktutorial.sh
@@ -5,7 +5,8 @@ pushd ./tools/mktutorial
 go run *.go https://github.com/pulumi/examples $TUTORIAL_OUT
 popd
 for cloud in "aws" "azure" "gcp" "kubernetes"; do
-    mkdir -p ./content/docs/tutorials/$cloud
-    cp $TUTORIAL_OUT/tutorials/$cloud/* ./content/docs/tutorials/$cloud/
+    mkdir -p ./content/registry/packages/$cloud
+    mkdir -p ./content/registry/packages/$cloud/how-to-guides
+    cp $TUTORIAL_OUT/tutorials/$cloud/* ./content/registry/packages/$cloud/how-to-guides/
 done
 rm -rf $TUTORIAL_OUT
