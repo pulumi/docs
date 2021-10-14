724f45df6774448fa74aee342c63c5afa481c727
Devon Grove <devon@pulumi.com>
2021-10-14 09:02:27 -0700

    Made enhancements to sitemap crawler

--- scripts/validate-sitemap-urls.sh
+++ scripts/validate-sitemap-urls.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+SITEMAP=/tmp/sitemap.xml
+URL_LIST=/tmp/site_urls.txt
+URL_RESPONSES=/tmp/url_responses.txt
+ERROR_LIST=/tmp/error_urls.txt
+
 request_and_format_sitemap() {
-    curl https://www.pulumi.com/sitemap.xml > sitemap.xml && xmllint --format sitemap.xml > temp_sitemap.xml && mv temp_sitemap.xml sitemap.xml
+    echo "Requesting pulumi.com sitemap..."
+    curl https://www.pulumi.com/sitemap.xml > $SITEMAP && xmllint --format $SITEMAP > /tmp/sitemap2.xml && mv /tmp/sitemap2.xml $SITEMAP
+    echo "done\n"
 }
 
 parse_urls() {
-    cat sitemap.xml | grep -e loc | sed 's|<loc>\(.*\)<\/loc>$|\1|g' | sed 's/www.pulumi.com/www-reg-staging.pulumi-dev.io/g' > site_urls.txt
+    echo "Building URL list..."
+    cat $SITEMAP | grep -e loc | sed 's|<loc>\(.*\)<\/loc>$|\1|g' | sed 's/www.pulumi.com/www-reg-staging.pulumi-dev.io/g' > $URL_LIST
 }
 
 request_urls() {
-    cat site_urls.txt | xargs -I {} curl -s -o /dev/null -w "%{http_code} %{url_effective}\n" {} | grep -v '200\|301'
+    cat $URL_LIST | xargs -I {} curl -s -o /dev/null -w "%{http_code} %{url_effective}\n" {} | tee $URL_RESPONSES
+    cat $URL_RESPONSES | grep -v '200\|301' > $ERROR_LIST
+    cat $URL_RESPONSES | grep -c '200' | echo "Number of 200 responses: $1"
+    cat $URL_RESPONSES | grep -c '301' | echo "Number of 301 responses: $1"
+    echo "THe following URLs returned errors:"
+    cat $ERROR_LIST
+}
+
+cleanup() {
+    rm $URL_LIST $SITEMAP $URL_RESPONSES $ERROR_LIST
 }
 
 request_and_format_sitemap
 parse_urls
 request_urls
+cleanup
