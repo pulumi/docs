a8917bbef99fbe135d05bc355710649f05ef51a4
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2021-10-15 17:08:15 -0700

    Update script

--- scripts/gen_component_docs.sh
+++ scripts/gen_component_docs.sh
@@ -2,46 +2,49 @@
 
 ensure_cloned() {
     local pkg=$1
-    pushd ../
-    if [ ! -d "${pkg}" ] ; then
+    # Go one-level outside the docs repo.
+    cd ../
+    if [ ! -d "pulumi-${pkg}" ] ; then
         echo "${pkg} doesn't exist. Cloning..."
-        git clone "https://github.com/pulumi-${pkg}" "${pkg}"
+        git clone "https://github.com/pulumi/pulumi-${pkg}"
     fi
-    popd
+    # Go back to the docs repo.
+    cd docs
 }
 
 ensure_cloned "aws-apigateway"
-./scripts/gen_resource_docs.sh "aws-apigateway" "" "/schema.json"
-./scripts/gen_package_metadata.sh "aws-apigateway" "/schema.json" "" "Pulumi" "" "" true
+./scripts/gen_resource_docs.sh "aws-apigateway" true "" "/schema.yaml"
+./scripts/gen_package_metadata.sh "aws-apigateway" "/schema.yaml" "" "Pulumi" "" "" true
 
 ensure_cloned "aws-miniflux"
-./scripts/gen_resource_docs.sh "aws-miniflux" "" "/schema.json"
+./scripts/gen_resource_docs.sh "aws-miniflux" true "" "/schema.json"
 ./scripts/gen_package_metadata.sh "aws-miniflux" "/schema.json" "" "Christian Nunciato" "" "infrastructure" true
 
 ensure_cloned "aws-quickstart-aurora-postgres"
-./scripts/gen_resource_docs.sh "aws-quickstart-aurora-postgres" "" "/schema.json"
-./scripts/gen_package_metadata.sh "aws-quickstart-aurora-postgres" "/schema.json" "" "Pulumi" "" "" true
+./scripts/gen_resource_docs.sh "aws-quickstart-aurora-postgres" true "" "/schema.yaml"
+./scripts/gen_package_metadata.sh "aws-quickstart-aurora-postgres" "/schema.yaml" "" "Pulumi" "" "" true
 
 ensure_cloned "aws-quickstart-redshift"
-./scripts/gen_resource_docs.sh "aws-quickstart-redshift" "" "/schema.json"
-./scripts/gen_package_metadata.sh "aws-quickstart-redshift" "/schema.json" "" "Pulumi" "" "" true
+./scripts/gen_resource_docs.sh "aws-quickstart-redshift" true "" "/schema.yaml"
+./scripts/gen_package_metadata.sh "aws-quickstart-redshift" "/schema.yaml" "" "Pulumi" "" "" true
 
 ensure_cloned "aws-quickstart-vpc"
-./scripts/gen_resource_docs.sh "aws-quickstart-vpc" "" "/schema.json"
-./scripts/gen_package_metadata.sh "aws-quickstart-vpc" "/schema.json" "" "Pulumi" "" "" true
+./scripts/gen_resource_docs.sh "aws-quickstart-vpc" true "" "/schema.yaml"
+./scripts/gen_package_metadata.sh "aws-quickstart-vpc" "/schema.yaml" "" "Pulumi" "" "" true
 
 ensure_cloned "aws-s3-replicated-bucket"
-./scripts/gen_resource_docs.sh "aws-s3-replicated-bucket" "" "/schema.json"
+./scripts/gen_resource_docs.sh "aws-s3-replicated-bucket" true "" "/schema.json"
 ./scripts/gen_package_metadata.sh "aws-s3-replicated-bucket" "/schema.json" "" "Lee Zen" "" "" true
 
 ensure_cloned "azure-quickstart-acr-geo-replication"
-./scripts/gen_resource_docs.sh "azure-quickstart-acr-geo-replication" "" "/schema.json"
+./scripts/gen_resource_docs.sh "azure-quickstart-acr-geo-replication" true "" "/schema.json"
 ./scripts/gen_package_metadata.sh "azure-quickstart-acr-geo-replication" "/schema.json" "" "Ian Wahbe" "" "" true
 
+#The schema.json for this component is in the default location of `provider/cmd/resource-pkg`.
 ensure_cloned "gcp-global-cloudrun"
-./scripts/gen_resource_docs.sh "gcp-global-cloudrun" "" "/schema.json"
-./scripts/gen_package_metadata.sh "gcp-global-cloudrun" "/schema.json" "" "Paul Stack" "" "" true
+./scripts/gen_resource_docs.sh "gcp-global-cloudrun"
+./scripts/gen_package_metadata.sh "gcp-global-cloudrun" "" "" "Paul Stack" "" "" true
 
 ensure_cloned "run-my-darn-container"
-./scripts/gen_resource_docs.sh "run-my-darn-container" "" "/schema.json"
+./scripts/gen_resource_docs.sh "run-my-darn-container" true "" "/schema.json"
 ./scripts/gen_package_metadata.sh "run-my-darn-container" "/schema.json" "" "Lee Briggs" "" "infrastructure" true
