d7a8444c5896aa452eb3e3a9430d5626b53ac308
Christian Nunciato <c@nunciato.org>
2021-10-16 20:11:37 -0700

    Remove the asset-build step (#746)

--- pulumi-hugo/scripts/build.sh
+++ pulumi-hugo/scripts/build.sh
@@ -4,6 +4,4 @@ set -o errexit -o pipefail
 
 source ./scripts/common.sh
 
-yarn --cwd themes/default run build
-
 hugo | grep -v -e 'WARN .* REF_NOT_FOUND'
--- pulumi-hugo/scripts/common.sh
+++ pulumi-hugo/scripts/common.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-BUILD_IDENTIFIER="$(git rev-parse HEAD)"
 export REPO_THEME_PATH="themes/default/"
-
-# Bundle filenames are exported for use with Hugo and theme scripts.
-export CSS_BUNDLE="static/css/styles.${BUILD_IDENTIFIER}.css"
-export JS_BUNDLE="static/js/bundle.min.${BUILD_IDENTIFIER}.js"
-export REL_CSS_BUNDLE="/css/styles.${BUILD_IDENTIFIER}.css"
-export REL_JS_BUNDLE="/js/bundle.min.${BUILD_IDENTIFIER}.js"
