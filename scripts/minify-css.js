c404c3ae24d3154556daa5e4f3a10c8b1f1f1487
Zack Chase <zachary@pulumi.com>
2022-03-29 09:46:55 -0700

    Support multiple bundles in minify (#7262)

--- scripts/minify-css.js
+++ scripts/minify-css.js
@@ -4,70 +4,80 @@ const postcss = require("postcss");
 const purgecss = require("@fullhuman/postcss-purgecss");
 const cssnano = require("cssnano");
 
-const bundlePath = glob.sync("public/css/bundle.*.css")[0];
-const css = fs.readFileSync(bundlePath);
+function minifyCSS(filePath) {
+    const bundlePath = glob.sync(filePath)[0];
 
-postcss([
-
-    // PurgeCSS removes unused CSS by analyzing the files of the built website.
-    // https://purgecss.com/
-    purgecss({
-        content: [
-            "public/**/*.html",
-            "public/js/bundle.*.js",
-        ],
-        css: [
-            bundlePath,
-        ],
-        safelist: {
-            deep: [
-                /^hs-/,
-                /^highlight$/,
-                /^pagination$/,
-                /^code-/,
-                /^copy-/,
-                /^carousel/,
-                /^st-/,
-                /^icon-/,
-                /^package-details/,
-                /^resources-properties/,
-                /^tabular/,
-                /^pulumi-/,
-            ],
-        },
-
-        // We need to extract the Tailwind screen size selectors (e.g. sm, md, lg)
-        // so that we do not strip them out. As long as a class name appears in the HTML
-        // in its entirety, PurgeCSS will not remove it.
-        // Ex. https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
-        defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-    }),
-
-    // CSSNano minifies our rendered CSS by removing whitespace, comments, etc.
-    // https://cssnano.co/
-    cssnano({
-        preset: [
-            "default",
-            {
-                discardComments: {
-                    removeAll: true,
-                },
-            },
-        ],
-    }),
-])
-.process(css, { from: bundlePath, to: bundlePath })
-.then(result => {
-    const css = result.css;
-
-    // Make sure there's at least some valid-looking CSS in the result.
-    if (!css || !css.match(/html|body/)) {
-        throw new Error(`Unexpected PostCSS result: ${css}`);
+    // If there is no matching bundle then we will skip minifying things.
+    if (bundlePath === undefined) {
+        return;
     }
 
-    // Write the result back to the file.
-    fs.writeFileSync(bundlePath, css);
-});
+    const css = fs.readFileSync(bundlePath);
+
+    postcss([
+
+        // PurgeCSS removes unused CSS by analyzing the files of the built website.
+        // https://purgecss.com/
+        purgecss({
+            content: [ "public/**/*.html", "public/js/bundle.*.js" ],
+            css: [
+                bundlePath,
+            ],
+            safelist: {
+                deep: [
+                    /^hs-/,
+                    /^highlight$/,
+                    /^pagination$/,
+                    /^code-/,
+                    /^copy-/,
+                    /^carousel/,
+                    /^st-/,
+                    /^icon-/,
+                    /^package-details/,
+                    /^resources-properties/,
+                    /^tabular/,
+                    /^pulumi-/,
+                ],
+            },
+
+            // We need to extract the Tailwind screen size selectors (e.g. sm, md, lg)
+            // so that we do not strip them out. As long as a class name appears in the HTML
+            // in its entirety, PurgeCSS will not remove it.
+            // Ex. https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
+            defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+        }),
+
+        // CSSNano minifies our rendered CSS by removing whitespace, comments, etc.
+        // https://cssnano.co/
+        cssnano({
+            preset: [
+                "default",
+                {
+                    discardComments: {
+                        removeAll: true,
+                    },
+                },
+            ],
+        }),
+    ])
+    .process(css, { from: bundlePath, to: bundlePath })
+    .then(result => {
+        const css = result.css;
+
+        // Make sure there's at least some valid-looking CSS in the result.
+        if (!css || !css.match(/html|body/)) {
+            throw new Error(`Unexpected PostCSS result: ${css}`);
+        }
+
+        // Write the result back to the file.
+        fs.writeFileSync(bundlePath, css);
+    });
+}
+
+Promise.all([
+    minifyCSS("public/css/bundle.*.css"),
+    minifyCSS("public/css/marketing.*.css"),
+]);
 
 // Exit non-zero when something goes wrong in the promise chain.
 process.on("unhandledRejection", error => {
