412701edd6bfb418eb3fdc96be59aea3246efc99
Kimberley Mackenzie <kimberley@pulumi.com>
2022-04-25 14:59:35 -0700

    Try turning gogc off.

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -34,9 +34,9 @@ popd
 printf "Running Hugo...\n\n"
 if [ "$1" == "preview" ]; then
     export HUGO_BASEURL="http://$(origin_bucket_prefix)-$(build_identifier).s3-website.$(aws_region).amazonaws.com"
-    GOGC=200 hugo --minify --templateMetrics -e "preview"
+    GOGC=off hugo --minify --templateMetrics -e "preview"
 else
-    GOGC=200 hugo --minify --templateMetrics -e production
+    GOGC=off hugo --minify --templateMetrics -e production
 fi
 
 # Purge unused CSS.
