db3c3fa1b5f9b30ffcbed01a32f059552d21f826
Christian Nunciato <c@nunciato.org>
2022-04-29 06:34:18 -0700

    GOGC 10

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -37,9 +37,9 @@ popd
 printf "Running Hugo...\n\n"
 if [ "$1" == "preview" ]; then
     export HUGO_BASEURL="http://$(origin_bucket_prefix)-$(build_identifier).s3-website.$(aws_region).amazonaws.com"
-    hugo --minify --templateMetrics -e "preview"
+    GOGC=10 hugo --minify --templateMetrics -e "preview"
 else
-    hugo --minify --templateMetrics -e production
+    GOGC=10 hugo --minify --templateMetrics -e production
 fi
 
 # Purge unused CSS.
