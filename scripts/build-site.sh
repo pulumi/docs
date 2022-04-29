8540fac44b00339cf67fb25224d5753e615c4b30
Christian Nunciato <c@nunciato.org>
2022-04-28 19:56:57 -0700

    Update build-site.sh

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -34,9 +34,9 @@ popd
 printf "Running Hugo...\n\n"
 if [ "$1" == "preview" ]; then
     export HUGO_BASEURL="http://$(origin_bucket_prefix)-$(build_identifier).s3-website.$(aws_region).amazonaws.com"
-    hugo --templateMetrics -e "preview"
+    hugo --minify --templateMetrics -e "preview"
 else
-    hugo --templateMetrics -e production
+    hugo --minify --templateMetrics -e production
 fi
 
 # Purge unused CSS.
