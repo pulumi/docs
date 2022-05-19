14a113b06a536a009c4649b734e73ba858ab412d
Christian Nunciato <c@nunciato.org>
2022-05-19 09:04:57 -0700

    Refactor the link-checking script

---
+++ scripts/link-checker/check-links.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -o nounset -o errexit -o pipefail
+
+source ./scripts/common.sh
+
+echo "Checking links..."
+
+base_url="$1"
+start_from_url="${2-""}"
+this_path="scripts/link-checker"
+pages_to_check=$(node "$this_path/get-pages-to-check.js" "$base_url" "$start_from_url")
+page_count="$(echo "$pages_to_check" | wc -l)"
+logfile="$this_path/pages-with-broken-links.txt"
+
+rm -f "$logfile"
+index=0
+
+for url in $pages_to_check
+do
+    index="$((index+1))"
+
+    echo "-------------------------------------------------------------------------------------------------------------"
+    echo "Checking $url ($index of ${page_count// }) ..."
+    echo "-------------------------------------------------------------------------------------------------------------"
+    echo ""
+
+    node "$this_path/check-links.js" "$url" "page" 3 || echo -e "$url" >> "$logfile"
+done
