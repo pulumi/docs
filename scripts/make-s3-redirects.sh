2cb88bc99f681a86f6ad4576480b346996583b5a
Christian Nunciato <c@nunciato.org>
2022-06-24 11:23:53 -0700

    Add region to redirects script (#7704)
    Overlooked this script in #7703.

--- scripts/make-s3-redirects.sh
+++ scripts/make-s3-redirects.sh
@@ -11,13 +11,13 @@ source ./scripts/common.sh
 build_dir="public"
 destination_bucket="$(cat "$(origin_bucket_metadata_filepath)" | jq -r ".bucket")"
 redirects_file="./redirects.txt"
-aws s3 cp "s3://${destination_bucket}/redirects.txt" "$redirects_file"
+aws s3 cp "s3://${destination_bucket}/redirects.txt" "$redirects_file" --region "$(aws_region)"
 
 echo "Processing S3 redirects for ${destination_bucket}..."
 IFS="|"
 while read key location; do
     echo "Redirecting $key to $location"
-    aws s3api put-object --key "$key" --website-redirect-location "$location" --bucket "$destination_bucket" --acl public-read
+    aws s3api put-object --key "$key" --website-redirect-location "$location" --bucket "$destination_bucket" --acl public-read --region "$(aws_region)"
 done < "$redirects_file"
 
 rm "$redirects_file"
