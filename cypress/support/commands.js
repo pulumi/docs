c6cef71d0d874cf05dc965325c8121862da8c8dc
zchase <zachary@pulumi.com>
2022-11-17 09:07:43 -0800

    Add handler to catch uncaught exceptions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    console.log("uncaught exception in cypress: ", err);
+    return false
+})
