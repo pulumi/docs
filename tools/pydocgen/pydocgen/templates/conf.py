489a6f58f0fbdcd16431a65946373ecdba5d6ab2
Kyle Dixler <kyle@pulumi.com>
2023-03-06 09:30:48 -0800

    Pydocgen was failing the release build due to dependency errors. Pinned `m2r<2.0.0` according to suggestion in https://github.com/Azure/azure-sdk-for-python/issues/22019#issue-1072490557 Pinned `markupsafe==2.0.1` according to discussion in https://github.com/SRserves85/avro-to-python/issues/20

--- tools/pydocgen/pydocgen/templates/conf.py
+++ tools/pydocgen/pydocgen/templates/conf.py
@@ -61,6 +61,9 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 
+# Permalinks are handled outside of sphinx.
+html_permalinks = False
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
