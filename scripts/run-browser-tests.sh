5e785e1e9a3545b3aa570a8829afb04d267d11f6
Sean Holung <sean.holung@gmail.com>
2023-03-10 11:20:35 -0800

    add cypress retries (#8678)

--- scripts/run-browser-tests.sh
+++ scripts/run-browser-tests.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-CYPRESS_BASE_URL="$1" yarn run cypress run --headless
+MAX_RETRIES=3
+RETRY_COUNT=0
+
+# Retry on errors up to `MAX_RETRIES` with a 10 second sleep in between.
+run_tests() {
+    while true; do
+        CYPRESS_BASE_URL="$1" yarn run cypress run --headless
+        exit_code=$?
+        if [ $exit_code -ne 0 ]; then
+            RETRY_COUNT=$((RETRY_COUNT+1))
+            if [ $RETRY_COUNT -eq $MAX_RETRIES ]; then
+                echo "Error: Browser tests failed after ${MAX_RETRIES} retries"
+                exit $exit_code
+            else
+                echo "Error: Retrying browser tests in 10 seconds..."
+                sleep 10
+            fi
+        else
+            break
+        fi
+    done
+}
+
+trap 'run_tests $1' EXIT
