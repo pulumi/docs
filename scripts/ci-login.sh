1eaeacd671839455f3d47d39624060b2f052310f
Sean Holung <sean.holung@gmail.com>
2023-03-27 16:28:47 -0700

    add role arn

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -10,8 +10,7 @@ if [[ "$GITHUB_WORKFLOW" == "Build and deploy" ]]; then
     pulumi -C infrastructure stack select pulumi/production
 elif [[ "$GITHUB_WORKFLOW" == "Build and deploy new account" ]]; then
     echo "Assuming the production CI role in new AWS account..."
-    # change this ARN to reference CI Role in new account.
-    eval $(assume-role -duration "2h0m0s" "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
+    eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
     # uncomment below when migration is complete and update stack selection to select the production stack
     # that manages the new environment.
     # pulumi login
