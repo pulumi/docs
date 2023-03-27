ed259e2ed91f800de6d569540605075fa7325a32
Sean Holung <sean.holung@gmail.com>
2023-03-27 11:32:06 -0700

    temporarily prevent indexing

--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -4,12 +4,17 @@ set -o errexit -o pipefail
 
 source ./scripts/ci-login.sh
 
-./scripts/build-site.sh
-./scripts/sync-and-test-bucket.sh
 
 if [[ "$GITHUB_WORKFLOW" == "Build and deploy" ]]; then
     # Wait for in-progress jobs to complete before proceeding.
+    ./scripts/build-site.sh
+    ./scripts/sync-and-test-bucket.sh
     node ./scripts/await-in-progress.js
     ./scripts/run-pulumi.sh update
+elif [[ "$GITHUB_WORKFLOW" == "Build and deploy new account" ]]; then
+    # temporarily setting site to build in preview mode, to prevent search bots from indexing
+    # until we cut over DNS to start routing to the new account.
+    ./scripts/build-site.sh preview
+    ./scripts/sync-and-test-bucket.sh
 fi
 ./scripts/make-s3-redirects.sh
