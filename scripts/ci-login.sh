e79cb97a702cb938e24c4135b06a035f48db592e
Sean Holung <sean.holung@gmail.com>
2023-04-03 10:49:34 -0700

    remove else if logic

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -2,21 +2,21 @@
 
 set -o errexit -o pipefail
 
-if [[ "$GITHUB_WORKFLOW" == "Build and deploy" ]]; then
+# if being run by the new account workflow, then assume role in new account.
+if [[ "$GITHUB_WORKFLOW" == "Build and deploy new account" ]]; then
+    echo "Assuming the production CI role in **NEW** AWS account..."
+    eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
+    
+    # uncomment pulumi login code below when migration is complete and update stack selection
+    # to select the production stack that manages the new environment.
+
+    # pulumi login
+    # pulumi -C infrastructure stack select pulumi/production
+else
     echo "Assuming the production CI role..."
     eval $(assume-role -duration "2h0m0s" "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
     echo "Selecting the pulumi/production stack"
     pulumi login
     pulumi -C infrastructure stack select pulumi/production
-elif [[ "$GITHUB_WORKFLOW" == "Build and deploy new account" ]]; then
-    echo "Assuming the production CI role in new AWS account..."
-    eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
-    # uncomment below when migration is complete and update stack selection to select the production stack
-    # that manages the new environment.
-    # pulumi login
-    # pulumi -C infrastructure stack select pulumi/production
-else
-    echo "the workflow, ${GITHUB_WORKFLOW}, is not recognized"
-    exit 1
 fi
 
