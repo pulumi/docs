988a850d0d3540364e5654e35ad83ca5e46e6989
Sean Holung <sean.holung@gmail.com>
2023-04-04 11:45:11 -0700

    add new prod stack

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -6,12 +6,9 @@ set -o errexit -o pipefail
 if [[ "$GITHUB_WORKFLOW" == "Build and deploy new account" ]]; then
     echo "Assuming the production CI role in **NEW** AWS account..."
     eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
-    
-    # uncomment pulumi login code below when migration is complete and update stack selection
-    # to select the production stack that manages the new environment.
 
-    # pulumi login
-    # pulumi -C infrastructure stack select pulumi/production
+    pulumi login
+    pulumi -C infrastructure stack select pulumi/www-production
 else
     echo "Assuming the production CI role..."
     eval $(assume-role -duration "2h0m0s" "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
