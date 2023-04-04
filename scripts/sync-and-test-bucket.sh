551de22305ebc79730088617875bc5ed3cfeb52f
Sean Holung <sean.holung@gmail.com>
2023-04-03 17:50:16 -0700

    test

--- scripts/sync-and-test-bucket.sh
+++ scripts/sync-and-test-bucket.sh
@@ -99,8 +99,10 @@ aws s3 cp "$metadata_file" "${destination_bucket_uri}/metadata.json" --region "$
 # Persist an association between the current commit and the bucket we just deployed to.
 set_bucket_for_commit "$(git_sha)" "$destination_bucket" "$(aws_region)"
 
+pwd
+
 # Set cors configuration on bucket.
-aws s3api put-bucket-cors --bucket "$destination_bucket" --cors-configuration file://cors.json
+aws s3api put-bucket-cors --bucket "$destination_bucket" --cors-configuration file://scripts/cors.json
 
 # Finally, if it's a preview, post a comment to the PR that directs the user to the resulting bucket URL.
 if [ "$1" == "preview" ]; then
