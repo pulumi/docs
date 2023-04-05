72e2a0f77e80d6346629843315ebb025270fcbc8
Sean Holung <sean.holung@gmail.com>
2023-04-04 17:21:12 -0700

    add region to command

--- scripts/sync-and-test-bucket.sh
+++ scripts/sync-and-test-bucket.sh
@@ -100,7 +100,7 @@ aws s3 cp "$metadata_file" "${destination_bucket_uri}/metadata.json" --region "$
 set_bucket_for_commit "$(git_sha)" "$destination_bucket" "$(aws_region)"
 
 # Set cors configuration on bucket.
-aws s3api put-bucket-cors --bucket "$destination_bucket" --cors-configuration file://scripts/cors/cors.json
+aws s3api put-bucket-cors --bucket "$destination_bucket" --cors-configuration "file://scripts/cors/cors.json" --region "$(aws_region)"
 
 # Finally, if it's a preview, post a comment to the PR that directs the user to the resulting bucket URL.
 if [ "$1" == "preview" ]; then
