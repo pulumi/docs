a28f1ccfe2ecf03c984578823974f0678cdbb0ac
susan evans <susan.ra.evans@gmail.com>
2023-04-20 13:39:50 -0700

    putting 403s back (#8923)

--- scripts/link-checker/check-links.js
+++ scripts/link-checker/check-links.js
@@ -256,9 +256,6 @@ function getDefaultExcludedKeywords() {
 // Filters out transient errors that needn't fail a link-check run.
 function excludeAcceptable(links) {
     return (links
-        // Ignore 403's
-        .filter(b => b.reason !== "HTTP_403")
-
         // Ignore GitHub and npm 429s (rate-limited). We should really be handling these more
         // intelligently, but we can come back to that in a follow up.
         .filter(b => !(b.reason === "HTTP_429" && b.destination.match(/github.com|npmjs.com/)))
