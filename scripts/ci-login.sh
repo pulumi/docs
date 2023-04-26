0e2cf78a47e3396ab72e8e658a1733296d5f7f4d
Sean Holung <sean.holung@gmail.com>
2023-04-26 14:10:24 -0700

    update code comment

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -2,10 +2,10 @@
 
 set -o errexit -o pipefail
 
-echo "Assuming the production CI role in **NEW** AWS account..."
+echo "Assuming the production CI role in AWS account..."
 eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
 
-echo "Selecting the pulumi/production stack"
+echo "Selecting the pulumi/www-production stack"
 pulumi login
 pulumi -C infrastructure stack select pulumi/www-production
 
