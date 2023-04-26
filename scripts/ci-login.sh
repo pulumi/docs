4631d2f326db96b6c5992c64d2d61ea388ef13f6
Sean Holung <sean.holung@gmail.com>
2023-04-26 13:41:40 -0700

    update scripts

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -2,18 +2,10 @@
 
 set -o errexit -o pipefail
 
-# if being run by the new account workflow, then assume role in new account.
-if [[ "$GITHUB_WORKFLOW" == "Build and deploy new account" ]]; then
-    echo "Assuming the production CI role in **NEW** AWS account..."
-    eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
+echo "Assuming the production CI role in **NEW** AWS account..."
+eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
 
-    pulumi login
-    pulumi -C infrastructure stack select pulumi/www-production
-else
-    echo "Assuming the production CI role..."
-    eval $(assume-role -duration "2h0m0s" "arn:aws:iam::058607598222:role/ContinuousIntegrationRole")
-    echo "Selecting the pulumi/production stack"
-    pulumi login
-    pulumi -C infrastructure stack select pulumi/production
-fi
+echo "Selecting the pulumi/production stack"
+pulumi login
+pulumi -C infrastructure stack select pulumi/www-production
 
--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -4,22 +4,9 @@ set -o errexit -o pipefail
 
 source ./scripts/ci-login.sh
 
+./scripts/build-site.sh
+./scripts/sync-and-test-bucket.sh
+node ./scripts/await-in-progress.js
+./scripts/run-pulumi.sh update
 
-if [[ "$GITHUB_WORKFLOW" == "Build and deploy" ]]; then
-    # Wait for in-progress jobs to complete before proceeding.
-    ./scripts/build-site.sh
-    ./scripts/sync-and-test-bucket.sh
-    node ./scripts/await-in-progress.js
-    ./scripts/run-pulumi.sh update
-elif [[ "$GITHUB_WORKFLOW" == "Build and deploy new account" ]]; then
-    # temporarily setting site to build in preview mode, to prevent search bots from indexing
-    # until we cut over DNS to start routing to the new account.
-    ./scripts/build-site.sh
-    ./scripts/sync-and-test-bucket.sh
-    node ./scripts/await-in-progress.js
-    ./scripts/run-pulumi.sh update
-else
-    echo "the workflow, ${GITHUB_WORKFLOW}, is not recognized"
-    exit 1
-fi
 ./scripts/make-s3-redirects.sh
