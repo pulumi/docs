f78a5cae590bbcbd286a27eefc337f3764d5d11a
Sean Holung <sean.holung@gmail.com>
2023-05-12 11:18:30 -0700

    use oidc for aws auth (#9014)

--- scripts/ci-login.sh
+++ scripts/ci-login.sh
@@ -2,9 +2,6 @@
 
 set -o errexit -o pipefail
 
-echo "Assuming the production CI role in AWS account..."
-eval $(assume-role -duration "2h0m0s" "arn:aws:iam::388588623842:role/ContinuousIntegrationRole")
-
 echo "Selecting the pulumi/www-production stack"
 pulumi login
 pulumi -C infrastructure stack select pulumi/www-production
