300272c37923b15ff1038a4f6833865ec8789b3a
Sean Holung <sean.holung@gmail.com>
2023-06-13 15:44:54 -0700

    test

--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -4,12 +4,12 @@ set -o errexit -o pipefail
 
 source ./scripts/ci-login.sh
 
-# ./scripts/build-site.sh
-# ./scripts/sync-and-test-bucket.sh update
+./scripts/build-site.sh
+./scripts/sync-and-test-bucket.sh update
 
 node ./scripts/await-in-progress.js
 
-# ./scripts/run-pulumi.sh update
+./scripts/run-pulumi.sh update
 # ./scripts/update-search-index.sh production
 
-# ./scripts/make-s3-redirects.sh
+./scripts/make-s3-redirects.sh
