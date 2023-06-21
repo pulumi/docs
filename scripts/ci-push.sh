08094e27171ffb0c92605197549b2adc0d8bbf41
Sean Holung <sean.holung@gmail.com>
2023-06-21 11:12:17 -0700

    test

--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -10,6 +10,6 @@ source ./scripts/ci-login.sh
 node ./scripts/await-in-progress.js
 
 ./scripts/run-pulumi.sh update
-# ./scripts/update-search-index.sh production
+./scripts/update-search-index.sh production
 
 ./scripts/make-s3-redirects.sh
