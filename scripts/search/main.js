88f4f6678cea86731472133c64165076b95187b4
Sean Holung <sean.holung@gmail.com>
2023-09-28 13:56:51 -0700

    remove search pieces

--- scripts/search/main.js
+++ scripts/search/main.js
@@ -1,20 +1,11 @@
 const fs = require("fs");
-const path = require("path");
-const crypto = require("crypto");
 const page = require("./page");
-const registry = require("./registry");
-const settings = require("./settings");
 const algoliasearch = require("algoliasearch");
 
 // As part of the Hugo build, we generate a JSON file at /index.json containing a record for every
 // page of the website. We use this file to generate primary search records for Algolia.
 const pathToFullSiteJSON = "./public/index.json";
 const hugoPageItems = JSON.parse(fs.readFileSync(pathToFullSiteJSON, "utf-8").toString());
-
-// As part of the Registry build, we also generate a collection of JSON files that we use to power
-// the API docs navigation. We use this file here as well to add resource listings, etc., to the
-// index to make them more findable by their type names and the like.
-const pathToRegistryPackagesJSON = "./public/registry/packages/navs";
 
 // Configuration values required for updating the Algolia index.
 const config = {
@@ -42,18 +33,10 @@ const primaryPageObjects = page.getPrimaryObjects(hugoPageItems);
 console.log(" ↳ Building secondary page objects...");
 const secondaryPageObjects = page.getSecondaryObjects(primaryPageObjects);
 
-// Generate a list of all Registry items -- modules, resources, functions, etc.
-console.log(" ↳ Building Registry resource objects...");
-const registryObjects = registry.getObjects(pathToRegistryPackagesJSON, hugoPageItems);
-
-// Remove any objects from primaryPageObjects that also exist in registryObjects (to de-dupe).
-const filteredPageObjects = primaryPageObjects.filter(o => registryObjects.find(ro => ro.href === o.href) === undefined);
-
 // Stitch these lists together into one tidy bundle.
 let allObjects = [
     ...filteredPageObjects,
     ...secondaryPageObjects,
-    ...registryObjects,
 ];
 
 // Generate array of objects related to docs. The primary objects are included here instead of the filtered objects,
@@ -68,22 +51,9 @@ let docsObjects = [
 // https://github.com/pulumi/registry/issues/2879
 allObjects = allObjects.filter(o => !o.href.includes("azure-native-v1"));
 
-// Gather up index settings, synonyms, and rules.
-const indexSettings = {
-    searchableAttributes: settings.getSearchableAttributes(),
-    attributesForFaceting: settings.getAttributesForFaceting(),
-    attributesToHighlight: settings.getAttributesToHighlight(),
-    customRanking: settings.getCustomRanking(),
-    ignorePlurals: true,
-};
-const indexSynonyms = settings.getSynonyms();
-const indexRules = settings.getRules();
-
 // Write the results, just so we have them.
 console.log(" ↳ Writing results...");
 fs.writeFileSync("./public/search-index.json", JSON.stringify(allObjects, null, 4));
-fs.writeFileSync("./public/search-index-settings.json", JSON.stringify({ indexSettings, indexSynonyms, indexRules }, null, 4));
-fs.writeFileSync("./public/search-index-docs.json", JSON.stringify(docsObjects, null, 4));
 console.log(" ↳ Done. ✨\n");
 
 // Update the Algolia index, including all page objects and index settings (like searchable
@@ -95,16 +65,6 @@ async function updateIndex(objects) {
         console.log(` ↳ Replacing all records in the '${ config.indexName }' index...`);
         const result = await algoliaIndex.replaceAllObjects(objects, { safe: true });
         console.log(`   ↳ ${result.objectIDs.length} records updated.`);
-
-        console.log(` ↳ Updating index settings...`)
-        await algoliaIndex.setSettings(indexSettings);
-
-        console.log(" ↳ Updating synonyms...")
-        await algoliaIndex.saveSynonyms(indexSynonyms, { replaceExistingSynonyms: true });
-
-        console.log(" ↳ Updating rules...")
-        await algoliaIndex.replaceAllRules(indexRules);
-
         console.log(" ↳ Done. ✨\n");
     }
     catch (error) {
