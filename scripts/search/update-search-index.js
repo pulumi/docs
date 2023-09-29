8a9b4eb0380d361a165b78235acc57d0930eaa91
Sean Holung <sean.holung@gmail.com>
2023-09-29 12:52:39 -0400

    filter out azure-native-v1 objects (#9954)

--- scripts/search/update-search-index.js
+++ scripts/search/update-search-index.js
@@ -53,6 +53,11 @@ async function publishIndex() {
         ...registryIndex,
     ];
 
+    // Temporary hack: Remove any references to `azure-native-v1`. This line can be
+    // removed once the azure-native-v1 package is removed from the Registry.
+    // https://github.com/pulumi/registry/issues/2879
+    allObjects = allObjects.filter(o => !o.href.includes("azure-native-v1"));
+
     // Gather up index settings, synonyms, and rules.
     const indexSettings = {
         searchableAttributes: settings.getSearchableAttributes(),
