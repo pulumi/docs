71e1278f179ebcf53e57c4176d31f0b420570f0e
Sean Holung <sean.holung@gmail.com>
2023-09-29 10:37:21 -0400

    update search index file name (#9952)

--- scripts/ci-update-search-index.sh
+++ scripts/ci-update-search-index.sh
@@ -2,4 +2,4 @@
 
 # This script downloads the search indexes for registry and docs from pulumi.com then combines
 # the two indexes and pushes them to Algolia.
-node ./scripts/search/update.js "$1"
+node ./scripts/search/update-search-index.js "$1"
