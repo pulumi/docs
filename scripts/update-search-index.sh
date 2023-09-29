76ea0f059f4dcf6b47e1bdd0258142ecce024b1c
Sean Holung <sean.holung@centurylink.com>
2023-09-29 11:38:05 -0700

    update filename

--- scripts/update-search-index.sh
+++ scripts/update-search-index.sh
@@ -12,4 +12,4 @@ node ./scripts/search/main.js "$1"
 destination_bucket="$(cat "$(origin_bucket_metadata_filepath)" | jq -r ".bucket")"
 
 # Upload the `search-index.json` file to S3 where it can be accessed by the update search index cron.
-aws s3 cp "./public/search-index-docs.json" "s3://${destination_bucket}/search-index.json" --acl public-read --region "$(aws_region)"
+aws s3 cp "./public/search-index.json" "s3://${destination_bucket}/search-index.json" --acl public-read --region "$(aws_region)"
