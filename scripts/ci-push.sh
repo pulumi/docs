342b10fe014e0f7fda9901ee0795706564814421
Sean Holung <sean.holung@centurylink.com>
2023-10-02 13:31:49 -0700

    write index file before update

--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -7,10 +7,11 @@ source ./scripts/ci-login.sh
 ./scripts/build-site.sh
 ./scripts/sync-and-test-bucket.sh update
 
-node ./scripts/await-in-progress.js
-
-./scripts/run-pulumi.sh update
 if [[ "$DEPLOYMENT_ENVIRONMENT" != "testing" ]]; then
     ./scripts/update-search-index.sh production
 fi
+
+node ./scripts/await-in-progress.js
+
+./scripts/run-pulumi.sh update
 ./scripts/make-s3-redirects.sh
