c04cf630829d65efe6005169052abf650743afaf
Christian Nunciato <chris@nunciato.org>
2023-10-06 22:15:21 -0700

    Process S3 redirects in parallel

--- scripts/make-s3-redirects.sh
+++ scripts/make-s3-redirects.sh
@@ -14,22 +14,10 @@ redirects_file="./redirects.txt"
 aws s3 cp "s3://${destination_bucket}/redirects.txt" "$redirects_file" --region "$(aws_region)"
 
 echo "Processing S3 redirects for ${destination_bucket}..."
-IFS="|"
-while read key location; do
-    echo "Redirecting $key to $location"
-    aws s3api put-object --key "$key" --website-redirect-location "$location" --bucket "$destination_bucket" --acl public-read --region "$(aws_region)"
-done < "$redirects_file"
+node scripts/make-s3-redirects.js "${destination_bucket}" "${build_dir}/redirects.txt" "$(aws_region)"
 
-rm "$redirects_file"
-
-# Apply custom redirects supplied in the `scripts/redirects` directory.
+echo "Processing custom redirects in scripts/redirects..."
 ls -l "./scripts/redirects/" | tail -n +2 | awk '{print $9}' | while read line; do
     redirect_file="./scripts/redirects/$line"
-    while read key location; do
-        # skip empty lines
-        if [[ ! -z "$key" ]]; then
-            echo "Redirecting $key to $location"
-            aws s3api put-object --key "$key" --website-redirect-location "$location" --bucket "$destination_bucket" --acl public-read --region "$(aws_region)"
-        fi
-    done < "$redirect_file"
+    node scripts/make-s3-redirects.js "${destination_bucket}" "${redirect_file}" "$(aws_region)"
 done
