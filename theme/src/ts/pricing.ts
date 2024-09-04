320440d8be6943d4a6e5f1b6e100778ca3dae17c
Sarah Hughes <sarah@pulumi.com>
2024-09-04 15:28:01 -0700

    update tab background

--- theme/src/ts/pricing.ts
+++ theme/src/ts/pricing.ts
@@ -5,10 +5,10 @@ $(function () {
         $(".iac-price").removeClass("hidden");
 
         // flip tab styles
-        $('#esc-pricing-select').removeClass("border-blue-600 bg-gray-100");
+        $('#esc-pricing-select').removeClass("border-blue-600 bg-violet-100");
         $('#esc-pricing-select').addClass("border-gray-300");
         $('#esc-pricing-select-text').removeClass("rainbow-text")
-        $('#iac-pricing-select').addClass("border-blue-600 bg-gray-100");
+        $('#iac-pricing-select').addClass("border-blue-600 bg-violet-100");
         $('#iac-pricing-select-text').addClass("rainbow-text")
         
     });
@@ -19,10 +19,10 @@ $(function () {
         $(".esc-price").removeClass("hidden");
 
         // flip tab styles
-        $('#iac-pricing-select').removeClass("border-blue-600 bg-gray-100");
+        $('#iac-pricing-select').removeClass("border-blue-600 bg-violet-100");
         $('#iac-pricing-select').addClass("border-gray-300");
         $('#iac-pricing-select-text').removeClass("rainbow-text")
-        $('#esc-pricing-select').addClass("border-blue-600 bg-gray-100");
+        $('#esc-pricing-select').addClass("border-blue-600 bg-violet-100");
         $('#esc-pricing-select-text').addClass("rainbow-text")
     });
 
