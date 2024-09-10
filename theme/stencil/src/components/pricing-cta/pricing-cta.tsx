222138f9405d9ecb9888cfe7950bc698b3bbc645
Sarah Hughes <sarah@pulumi.com>
2024-09-10 16:00:04 -0700

    Revert "add button class"
    This reverts commit f90c19a92252bfd30b468aca88313cf23ba11469.

--- theme/stencil/src/components/pricing-cta/pricing-cta.tsx
+++ theme/stencil/src/components/pricing-cta/pricing-cta.tsx
@@ -5,7 +5,7 @@ import { parseCookie } from "../../util/util";
     tag: "pricing-cta",
     shadow: false,
 })
-export class pricingCta {
+export class HeaderCta {
     @Prop()
     buttonClass = "";
 
