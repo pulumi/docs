2691be2686159e9a9ade3f27cb4a51dca28c68c2
Christian Nunciato <chris@nunciato.org>
2024-09-11 09:28:01 -0700

    Bring in the expandable/collapsible behavior

---
+++ theme/stencil/src/components/docs-nav/docs-nav.tsx
@@ -0,0 +1,35 @@
+import { Component, h, Element, Listen } from "@stencil/core";
+
+@Component({
+    tag: "pulumi-docs-nav",
+    styleUrl: "docs-nav.scss",
+    shadow: false,
+})
+export class DocsNav {
+    @Element()
+    el: HTMLElement;
+
+    @Listen("click")
+    onClick(event: MouseEvent) {
+        const target = event.target as HTMLElement;
+
+        if (target.nodeName.toLowerCase() === "button") {
+            target.closest("li").classList.toggle("collapsed");
+            target.closest("li").classList.toggle("expanded");
+            event.stopPropagation();
+            event.preventDefault();
+        }
+    }
+
+    componentDidRender() {
+        this.el.querySelectorAll("li > ul").forEach(item => {
+            const button = document.createElement("button");
+            const parent = item.parentElement.querySelector("a");
+            parent.appendChild(button);
+        });
+    }
+
+    render() {
+        return <slot></slot>;
+    }
+}
