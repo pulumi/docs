ee0b198d8a3d7390974c24d0a69095285dcdacd4
Christian Nunciato <chris@nunciato.org>
2024-09-12 15:53:44 -0700

    Spruce up the nav

--- theme/stencil/src/components/docs-nav/docs-nav.tsx
+++ theme/stencil/src/components/docs-nav/docs-nav.tsx
@@ -13,8 +13,8 @@ export class DocsNav {
     onClick(event: MouseEvent) {
         const target = event.target as HTMLElement;
 
-        if (target.nodeName.toLowerCase() === "button") {
-            target.closest("li").classList.toggle("collapsed");
+        if (target.nodeName.toLowerCase() === "button" || target.nodeName.toLowerCase() === "i") {
+            // target.closest("li").classList.toggle("collapsed");
             target.closest("li").classList.toggle("expanded");
             event.stopPropagation();
             event.preventDefault();
@@ -22,11 +22,47 @@ export class DocsNav {
     }
 
     componentDidRender() {
-        this.el.querySelectorAll("li > ul").forEach(item => {
-            const button = document.createElement("button");
-            const parent = item.parentElement.querySelector("a");
-            parent.appendChild(button);
+        this.el.querySelectorAll("li > a").forEach(item => {
+
+            const a = item.parentElement.querySelector("a");
+            const label = a.textContent;
+            a.textContent = "";
+
+            const span = document.createElement("span");
+            span.textContent = label;
+
+            a.appendChild(span);
         });
+
+        this.el.querySelectorAll("li > ul").forEach(item => {
+
+            const a = item.parentElement.querySelector("a");
+            const label = a.textContent;
+            a.textContent = "";
+
+            const button = document.createElement("button");
+            const i = document.createElement("i");
+            i.classList.add("fa", "fa-caret-right");
+            button.appendChild(i);
+
+            const span = document.createElement("span");
+            span.textContent = label;
+
+            // a.closest("li").classList.add("expandable");
+            a.appendChild(button);
+            a.appendChild(span);
+        });
+
+        const activeItem = this.el.querySelector("a.active");
+        if (activeItem) {
+            console.log(({ activeItem }));
+            let parent = activeItem.closest("pulumi-docs-nav li.expandable");
+            console.log({ parent });
+            while (parent) {
+                parent.classList.add("expanded");
+                parent = parent.parentElement.closest("pulumi-docs-nav li.expandable");
+            }
+        }
     }
 
     render() {
