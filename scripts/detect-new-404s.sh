9b4efd958229ade84b3a25a3aa01e8d6167e376c
Christian Nunciato <chris@nunciato.org>
2024-09-13 16:30:55 -0700

    Don't limit the 404 check to docs

--- scripts/detect-new-404s.sh
+++ scripts/detect-new-404s.sh
@@ -10,4 +10,4 @@ destination_bucket="$(cat "$(origin_bucket_metadata_filepath)" | jq -r ".bucket"
 s3_website_url="http://${destination_bucket}.s3-website.$(aws_region).amazonaws.com"
 
 echo "Checking ${s3_website_url} for new 404s..."
-node scripts/detect-new-404s.js "https://pulumi.com" "${s3_website_url}" "/docs"
+node scripts/detect-new-404s.js "https://pulumi.com" "${s3_website_url}"
