e5f3f9fc0c1e2536350df3a33fc00931f57bf4fb
Anton Tayanovskyy <anton@pulumi.com>
2024-10-02 13:27:09 -0400

    Fix aws-s3-bucket-resources-go

--- static/programs/aws-s3-bucket-resources-go/main.go
+++ static/programs/aws-s3-bucket-resources-go/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+		bucket, err := s3.NewBucketV2(ctx, "my-bucket", nil)
 		if err != nil {
 			return err
 		}
