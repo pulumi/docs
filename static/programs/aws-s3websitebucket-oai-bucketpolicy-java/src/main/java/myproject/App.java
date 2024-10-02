ddabc5dc960e99e81b011a922656c96dcf588d0b
Anton Tayanovskyy <anton@pulumi.com>
2024-10-02 15:35:01 -0400

    Fix aws-s3websitebucket-oai-bucketpolicy-java

--- static/programs/aws-s3websitebucket-oai-bucketpolicy-java/src/main/java/myproject/App.java
+++ static/programs/aws-s3websitebucket-oai-bucketpolicy-java/src/main/java/myproject/App.java
@@ -7,7 +7,7 @@ import com.pulumi.aws.s3.Bucket;
 public class App {
     public static void main(String[] args) {
         Pulumi.run(ctx -> {
-            var bucket = new Bucket("my-bucket");
+            var bucket = new BucketV2("my-bucket");
             ctx.export("bucketName", bucket.bucket());
         });
     }
