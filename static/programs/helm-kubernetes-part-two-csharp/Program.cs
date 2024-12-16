5ee7ae5b5eb1bf2465e801716cc46650e72ee7d7
Engin Diri <engin.diri@ediri.de>
2024-12-16 10:45:19 +0100

    feat: Install Helm Charts using the Chart resource tutorial

---
+++ static/programs/helm-kubernetes-part-two-csharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Pulumi;
+using Pulumi.Experimental.Provider;
+using Pulumi.Kubernetes.Helm.V4;
+using Pulumi.Kubernetes.Types.Inputs;
+using Pulumi.Kubernetes.Types.Inputs.Helm.V4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+await Deployment.RunAsync<MyStack>();
+
+
+class MyStack : Stack
+{
+    public MyStack() 
+    {
+        var certman = new Chart("cert-manager", new ChartArgs
+        {
+            Namespace = "cert-manager",
+            Chart = "oci://registry-1.docker.io/bitnamicharts/cert-manager",
+            Version = "1.3.1",
+        }, 
+        new ComponentResourceOptions
+        {
+            ResourceTransforms =
+            {
+               async (args, _) => 
+                {
+                    Console.WriteLine("Transforming resource");
+                    if (args.Type != "kubernetes:helm.sh/v4:Chart")
+                    {
+                    var myTags = ImmutableDictionary.Create<string, object>().Add("cost-center", "12345");
+
+                    InputMap<object> tags =
+                    args.Args.TryGetValue("metadata", out var tagsValue) && tagsValue is not null ?
+                        tagsValue is Output<ImmutableDictionary<string, object>> tagsOutput ? tagsOutput :
+                        tagsValue is ImmutableDictionary<string, object> tagsDictionary ? tagsDictionary :
+                        throw new InvalidOperationException($"Unexpected tags type: {tagsValue.GetType()}") :
+                    ImmutableDictionary<string, object>.Empty;
+
+                    tags = tags.Apply(t => t.SetItems(myTags));
+                    return new( args.Args.SetItem("metadata", tags), args.Options);
+                    }
+                    return null;
+                },
+            },
+        });
+    }
+}
