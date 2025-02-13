6c818cbc0994f9ac683a055d8d6c4e94246b0125
Ian Wahbe <me@iwahbe.com>
2025-02-13 15:13:03 +0100

    Run the typechecker on `scripts`
    This would have caught #14051.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,3 +4,5 @@ set -o errexit -o pipefail
 
 yarn run lint-markdown
 yarn prettier --check .
+# shellcheck disable=SC2046 # Intended splitting of file names
+yarn tsc --project tsconfig.scripts.json --noEmit
