0a8038dfe2c32c283846e110870e7defaacadc7f
Ian Wahbe <me@iwahbe.com>
2025-02-24 19:24:00 +0100

    Revert "Run the typechecker on `scripts` (#14069)"
    This reverts commit 9ead6bf4875f72251a8c1b8c424862649d495438.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,5 +4,3 @@ set -o errexit -o pipefail
 
 yarn run lint-markdown
 yarn prettier --check .
-# shellcheck disable=SC2046 # Intended splitting of file names
-yarn tsc --project tsconfig.scripts.json --noEmit
