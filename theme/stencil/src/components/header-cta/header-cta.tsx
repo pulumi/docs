d69f5abc9ecbcb381d8d2c2a33b330ac4c0d498f
Christian Nunciato <chris@nunciato.org>
2026-02-25 17:38:43 -0800

    Land signed-in users on the dashboard, not /signup

--- theme/stencil/src/components/header-cta/header-cta.tsx
+++ theme/stencil/src/components/header-cta/header-cta.tsx
@@ -45,12 +45,12 @@ export class HeaderCta {
 
         if (this.isLoggedIn) {
             return(
-                <a class={this.buttonClass} data-track="header-signup" href="https://app.pulumi.com/signup?utm_source=header-button" title="Dashboard">Dashboard</a>
+                <a class={this.buttonClass} href="https://app.pulumi.com/?utm_source=header-button" title="Dashboard">Dashboard</a>
             );
         }
 
         return (
-            <a class={this.buttonClass} data-track="header-signup" data-role="cta-get-started" href={this.href} title={this.label}>{this.label}</a>
+            <a class={this.buttonClass} data-track="header-signup" data-role="cta-get-started" href={`${this.href}?utm_source=header-button`} title={this.label}>{this.label}</a>
         );
     }
 
